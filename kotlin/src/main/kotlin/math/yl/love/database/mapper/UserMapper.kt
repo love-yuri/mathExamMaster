a9c65a4a13daf7c3531618a31462f039897c93bc
love-yuri <yuri2078170658@gmail.com>
2024-08-06 01:05:45 +0800

    feat(UserMapper): 添加根据ID查询用户的功能

--- kotlin/src/main/kotlin/math/yl/love/database/mapper/UserMapper.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/mapper/UserMapper.kt
@@ -1,8 +1,14 @@
 package math.yl.love.database.mapper
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
 import math.yl.love.database.entity.User
 import org.apache.ibatis.annotations.Mapper
 
 @Mapper
-interface UserMapper: BaseMapper<User>
+interface UserMapper: BaseMapper<User> {
+    fun findBy(): User{
+        return selectOne(LambdaQueryWrapper<User>()
+            .eq(User::id, 1))
+    }
+}
