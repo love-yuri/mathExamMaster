3eb297dee5c41b07ce81d781f52db76ed08063ae
love-yuri <yuri2078170658@gmail.com>
2024-08-06 01:05:22 +0800

    feat: 添加了SqlCondition.EQUAL导入语句

--- kotlin/src/main/kotlin/math/yl/love/base/mybatis/BaseEntity.kt
+++ kotlin/src/main/kotlin/math/yl/love/base/mybatis/BaseEntity.kt
@@ -2,6 +2,7 @@ package math.yl.love.base.mybatis
 
 import com.baomidou.mybatisplus.annotation.FieldFill
 import com.baomidou.mybatisplus.annotation.IdType
+import com.baomidou.mybatisplus.annotation.SqlCondition.EQUAL
 import com.baomidou.mybatisplus.annotation.TableField
 import com.baomidou.mybatisplus.annotation.TableId
 import com.baomidou.mybatisplus.annotation.TableLogic
