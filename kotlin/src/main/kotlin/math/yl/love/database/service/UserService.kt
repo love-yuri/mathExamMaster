0fbc14a0c0ea41da2f0bb3d2d197277425d6bef2
love-yuri <yuri2078170658@gmail.com>
2024-08-06 01:05:49 +0800

    feat(UserService): 添加事务处理和查询功能

--- kotlin/src/main/kotlin/math/yl/love/database/service/UserService.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/service/UserService.kt
@@ -1,18 +1,25 @@
-package math.yl.love.database.mapper
+package math.yl.love.database.service
 
+import com.baomidou.mybatisplus.extension.kotlin.KtQueryWrapper
+import math.yl.love.base.mybatis.BaseEntity
 import math.yl.love.base.mybatis.BaseService
 import math.yl.love.database.entity.User
+import math.yl.love.database.mapper.UserMapper
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class UserService(
+@Transactional(readOnly = true)
+class UserService : BaseService<User, UserMapper>() {
 
-): BaseService<User, UserMapper>() {
+    override val queryWrapper: KtQueryWrapper<User> get() = KtQueryWrapper(User())
 
     fun test() {
         val all = list().joinToString {
             "id: ${it.id} info: $it"
         }
+
         logger.info("yuri: 获取结果: $all")
+        logger.info("yuri: 结果 -> ${list(queryWrapper.eq(BaseEntity::id, 1))}")
     }
 }
