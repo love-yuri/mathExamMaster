6cb8a6ff3c7ef5fa865bfecf6c8787649c45d1b2
love-yuri <yuri2078170658@gmail.com>
2024-08-06 01:05:34 +0800

    feat(TestController): 更新了UserService的导入路径

--- kotlin/src/main/kotlin/math/yl/love/controller/TestController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/TestController.kt
@@ -1,6 +1,6 @@
 package math.yl.love.controller
 
-import math.yl.love.database.UserService
+import math.yl.love.database.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
