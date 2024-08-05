87f4ff31709c170d2a6dc03847c9752efaa8e202
love-yuri <yuri2078170658@gmail.com>
2024-08-06 01:05:17 +0800

    feat: 创建基础控制器类BaseController

---
+++ kotlin/src/main/kotlin/math/yl/love/base/mybatis/BaseController.kt
@@ -0,0 +1,24 @@
+package math.yl.love.base.mybatis
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+
+
+abstract class BaseController<Entity: BaseEntity, Mapper: BaseMapper<Entity>> {
+
+    @Autowired
+    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+    private lateinit var baseService: BaseService<Entity, Mapper>
+
+    @PostMapping("create")
+    fun create(@RequestBody value: Entity) = baseService.create(value)
+
+    @PostMapping("update")
+    fun update(@RequestBody value: Entity) = baseService.update(value)
+
+    @PostMapping("delete/{id}")
+    fun update(@PathVariable id: Long) = baseService.delete(id)
+}
