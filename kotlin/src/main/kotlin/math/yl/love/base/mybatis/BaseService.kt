b43913ad0fdd77f265dfd7882a53d6c6c5845d6a
love-yuri <yuri2078170658@gmail.com>
2024-08-06 01:05:29 +0800

    feat: 添加BaseService抽象类以实现Mybatis服务功能

--- kotlin/src/main/kotlin/math/yl/love/base/mybatis/BaseService.kt
+++ kotlin/src/main/kotlin/math/yl/love/base/mybatis/BaseService.kt
@@ -1,7 +1,45 @@
 package math.yl.love.base.mybatis
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
+import com.baomidou.mybatisplus.extension.kotlin.KtQueryWrapper
+import com.baomidou.mybatisplus.extension.service.IService
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl
+import math.yl.love.database.entity.User
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-abstract class BaseService <T: BaseEntity>(
 
-)
+abstract class BaseService <Entity: BaseEntity, Mapper: BaseMapper<Entity>> : ServiceImpl<Mapper, Entity>(), IService<Entity> {
+
+    /**
+     * 快捷lambda Query查询类
+     */
+    protected abstract val queryWrapper: KtQueryWrapper<Entity>
+
+    /**
+     * 日志类
+     * 因为和本地的log冲突，所以这里改用logger
+     */
+    protected val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    /**
+     * 创建
+     */
+    fun create(entity: Entity) = baseMapper.insert(entity)
+
+    /**
+     * 更新
+     */
+    fun update(entity: Entity) = baseMapper.updateById(entity)
+
+    /**
+     * 删除
+     */
+    fun delete(id: Long) = baseMapper.deleteById(id)
+
+
+
+}
+
