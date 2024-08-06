f8274198760c908d49f4a61c7509aeab5ea383a4
love-yuri <yuri2078170658@gmail.com>
2024-08-07 01:00:59 +0800

    feat: 更新KotlinApplication类以启用WebMvc

--- kotlin/src/main/kotlin/math/yl/love/KotlinApplication.kt
+++ kotlin/src/main/kotlin/math/yl/love/KotlinApplication.kt
@@ -1,15 +1,16 @@
 package math.yl.love
 
-import math.yl.love.configuration.SystemConfig
+import math.yl.love.configuration.config.SystemConfig
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
 @EnableConfigurationProperties(SystemConfig::class)
 @MapperScan("math.yl.love.database.mapper")
-open class KotlinApplication
+class KotlinApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinApplication>(*args)
