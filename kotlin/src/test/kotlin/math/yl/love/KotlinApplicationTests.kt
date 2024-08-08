9518420fa260d19c22ace7874dc087b923c10757
love-yuri <yuri2078170658@gmail.com>
2024-08-09 01:02:10 +0800

    feat(KotlinApplicationTests): 添加密码序列化测试用例

--- kotlin/src/test/kotlin/math/yl/love/KotlinApplicationTests.kt
+++ kotlin/src/test/kotlin/math/yl/love/KotlinApplicationTests.kt
@@ -1,13 +1,29 @@
-package yl.love
+package math.yl.love
 
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootTest
-class KotlinApplicationTests {
+class KotlinApplicationTests(
 
+) {
+    private val log = LoggerFactory.getLogger(KotlinApplicationTests::class.java)
+
+    /**
+     * 测试密码序列化器
+     */
     @Test
-    fun contextLoads() {
+    fun testPasswordEncode() {
+        val encoder = BCryptPasswordEncoder()
+        val testPwd = "yuri"
+        val encoded = encoder.encode(testPwd)
+        log.info("序列化后的结果: $encoded")
+        assert(encoder.matches(testPwd, encoded)) {
+            "密码不匹配"
+        }
+        log.info("检测密码是否匹配： ${encoder.matches(testPwd, encoded)}")
     }
 
 }
