8ceea55025f89f170381387e4b4269f78176e3c9
love-yuri <yuri2078170658@gmail.com>
2024-08-09 01:01:46 +0800

    feat(handler): 增加AccessDeniedHandler拒绝访问处理

--- kotlin/src/main/kotlin/math/yl/love/configuration/handler/AccessDeniedHandler.kt
+++ kotlin/src/main/kotlin/math/yl/love/configuration/handler/AccessDeniedHandler.kt
@@ -3,6 +3,9 @@ package math.yl.love.configuration.handler
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import math.yl.love.base.Log.log
+import math.yl.love.base.R
+import math.yl.love.base.SystemCode
+import math.yl.love.base.utils.JsonUtils.toJson
 import org.springframework.security.web.access.AccessDeniedHandler
 
 class AccessDeniedHandler : AccessDeniedHandler {
@@ -12,11 +15,12 @@ class AccessDeniedHandler : AccessDeniedHandler {
         response: HttpServletResponse?,
         accessDeniedException: org.springframework.security.access.AccessDeniedException?
     ) {
-
         response?.status = HttpServletResponse.SC_FORBIDDEN
         response?.contentType = "application/json;charset=UTF-8"
-        response?.writer?.write("{\"message\": \"Access Denied\"}")
+        val res = R.fail(SystemCode.AccessDenied.code, SystemCode.AccessDenied.message)
 
-        log.info("yuri: 访问拦截:")
+        log.info("yuri: request -> ${request?.requestURI}")
+        response?.writer?.write(res.toJson())
     }
 }
+
