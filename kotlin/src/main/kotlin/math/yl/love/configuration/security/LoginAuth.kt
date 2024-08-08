201c57eb06c46228db262576f412f57dd70e335e
love-yuri <yuri2078170658@gmail.com>
2024-08-09 01:01:55 +0800

    feat(security): 更新登录认证逻辑

--- kotlin/src/main/kotlin/math/yl/love/configuration/security/LoginAuth.kt
+++ kotlin/src/main/kotlin/math/yl/love/configuration/security/LoginAuth.kt
@@ -1,18 +1,20 @@
 package math.yl.love.configuration.security
 
 import math.yl.love.base.Log.log
-import math.yl.love.database.entity.AutoUserInfo
+import math.yl.love.database.entity.DetailUserInfo
+import math.yl.love.database.service.UserService
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class LoginAuth: UserDetailsService {
+class LoginAuth(
+    private val userService: UserService
+): UserDetailsService {
     override fun loadUserByUsername(username: String?): UserDetails {
-        log.info("yuri: 获取用户名: $username")
-//        if (username == "yuri") {
-//            throw RuntimeException("密码错误")
-//        }
-        return AutoUserInfo()
+        val user = userService.getById(username?.toLong()) ?: run {
+            throw RuntimeException("不存在该用户: $username")
+        }
+        return DetailUserInfo(user)
     }
 }
