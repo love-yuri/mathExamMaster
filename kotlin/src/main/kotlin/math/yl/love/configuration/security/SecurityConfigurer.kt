1c1b326402e3a615be168aa98656b05b7f846959
love-yuri <yuri2078170658@gmail.com>
2024-08-09 01:01:59 +0800

    feat: 更新安全配置，增加密码加密和访问控制

--- kotlin/src/main/kotlin/math/yl/love/configuration/security/SecurityConfigurer.kt
+++ kotlin/src/main/kotlin/math/yl/love/configuration/security/SecurityConfigurer.kt
@@ -8,33 +8,46 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-open class SecurityConfigurer(
+class SecurityConfigurer(
     val systemConfig: SystemConfig
 ) {
 
-    @Bean
-    open fun securityFilterChain(http: HttpSecurity, authenticationConfiguration: AuthenticationConfiguration): SecurityFilterChain {
-        val securityIgnoreUrls = systemConfig.securityIgnoreUrls
-        val ignores = securityIgnoreUrls?.toTypedArray() ?: emptyArray()
+    @Bean fun PasswordEncoder (): PasswordEncoder = BCryptPasswordEncoder()
 
-        http
-            /* 授权认证 */
-            .authorizeHttpRequests {
-                it.requestMatchers(*ignores).permitAll() // 安全连接直接放行
-                it.requestMatchers("/**").permitAll() // 禁止放行
-                it.anyRequest().permitAll() // 不匹配，默认放行
-            }
-
-            /* 异常处理 */
-            .exceptionHandling {
-                it.accessDeniedHandler(AccessDeniedHandler())
-            }
-
-        return http.build()
-    }
+//    @Bean
+//    fun securityFilterChain(http: HttpSecurity, authenticationConfiguration: AuthenticationConfiguration): SecurityFilterChain {
+//        val securityIgnoreUrls = systemConfig.securityIgnoreUrls
+//        val ignores = securityIgnoreUrls?.toTypedArray() ?: emptyArray()
+//
+//        http
+//            /* 授权认证 */
+////            .authorizeHttpRequests {
+////                it.requestMatchers(*ignores).permitAll() // 安全连接直接放行
+////                it.requestMatchers("/doc.html", "/webjars/**", "v3/api-docs/**").permitAll() // 允许Knife4j相关路径
+////                it.anyRequest().authenticated()
+////            }
+////
+////            .logout {
+////                it.permitAll()
+////            }
+////
+//            .csrf {
+//                it.disable()
+//            }
+//
+//            /* 异常处理 */
+//            .exceptionHandling {
+//                it.accessDeniedHandler(AccessDeniedHandler())
+//            }
+//
+//        return http.build()
+//    }
 
 }
