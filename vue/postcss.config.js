ecc52a3e4517cc22d170966ff660c492b172d97c
love-yuri <yuri2078170658@gmail.com>
2024-08-10 17:02:42 +0800

    feat: 添加 postcss 和 tailwindcss 配置

---
+++ vue/postcss.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-08-10 16:57:20
+ * @LastEditTime: 2024-08-10 16:57:26
+ * @Description: postcss配置
+ */
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
