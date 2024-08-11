1609f526d5385b4c853a27478d529e3fd4e068a8
love-yuri <yuri2078170658@gmail.com>
2024-08-12 00:51:00 +0800

    feat(UserController): 添加用户登录功能

--- kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
@@ -1,13 +1,26 @@
 package math.yl.love.controller
 
-import math.yl.love.base.mybatis.BaseController
-import math.yl.love.database.entity.User
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport
+import math.yl.love.common.base.R
+import math.yl.love.common.mybatis.BaseController
+import math.yl.love.database.entity.entity.User
 import math.yl.love.database.mapper.UserMapper
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/user")
-class UserController: BaseController<User, UserMapper>() {
-    
+class UserController(
+    val authenticationManager: AuthenticationManager
+): BaseController<User, UserMapper>() {
+
+    @ApiOperationSupport(author = "xiaoymin@foxmail.com")
+    @PostMapping("/login")
+    fun login(@RequestBody user: User): R<User?> {
+        log.info("yuri: 获取的参数")
+        return R.success(User())
+    }
 }
