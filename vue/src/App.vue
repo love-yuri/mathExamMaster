b90a20b3a8720c2299a5b3ada5213db81f6b6b6e
love-yuri <yuri2078170658@gmail.com>
2024-08-12 00:51:43 +0800

    feat(App.vue): 初始化Toast功能并注册进pinia

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,36 @@
+<!--
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-08-10 15:47:59
+ * @LastEditTime: 2024-08-11 20:18:25
+ * @Description: App.vue
+-->
 <template>
+  <Toast />
   <RouterView />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import Toast from 'primevue/toast';
+import { useToastStore } from '@/stores/toast';
+import { useToast } from 'primevue/usetoast';
+
+/**
+ * 初始化Toast
+ * 将 toast实例注册进pinia中
+ */
+function initToast(): void {
+  const toastStore = useToastStore();
+  const toast = useToast();
+  toastStore.init(toast);
+}
+
+/**
+ * 初始函数，用于初始化全局的组件
+ */
+function init(): void {
+  initToast();
+}
+
+onMounted(init);
+</script>
