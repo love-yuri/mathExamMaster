e67df701074965fbe481bd4993a6ea46bf991bd8
love-yuri <yuri2078170658@gmail.com>
2024-08-11 16:04:52 +0800

    feat: 修改计数器store逻辑

--- vue/src/stores/counter.ts
+++ vue/src/stores/counter.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-08-10 15:47:59
- * @LastEditTime: 2024-08-11 01:04:43
+ * @LastEditTime: 2024-08-11 15:50:40
  * @Description: store 示例
  */
 import { ref, computed } from 'vue';
@@ -10,7 +10,7 @@ import { defineStore } from 'pinia';
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0);
   const doubleCount = computed(() => count.value * 2);
-  function increment() {
+  function increment(): void {
     count.value++;
   }
 
