8ecca4fd41e820507f203f5aab3a60b40ea65319
love-yuri <yuri2078170658@gmail.com>
2024-08-12 00:53:46 +0800

    feat(HomeView): 添加点击事件到按钮

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,17 +1,24 @@
 <!--
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-08-10 16:02:45
- * @LastEditTime: 2024-08-11 00:40:41
+ * @LastEditTime: 2024-08-12 00:30:58
  * @Description: 主页面
 -->
 <template>
   <h1 class="text-[40px]">{{ baseUrl }}</h1>
-  <Button>sfsdfds</Button>
+  <Button @click="showv">sfsdfds</Button>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import { Button } from '@/components';
+import { userApi } from '@/api/userApi';
+// import message from '@/common/utils/message';
 
 const baseUrl = ref(import.meta.env.VITE_BASE_URL);
+
+async function showv() {
+  const res = await userApi.login({});
+  console.log('res', res);
+}
 </script>
