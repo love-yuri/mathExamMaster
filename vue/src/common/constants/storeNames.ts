32c45e1afd6efdf36b3f3d257cddd1fdc3fdacac
love-yuri <yuri2078170658@gmail.com>
2024-08-12 00:53:15 +0800

    feat: 添加全局store名称常量

---
+++ vue/src/common/constants/storeNames.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-08-11 19:11:16
+ * @LastEditTime: 2024-08-12 00:52:24
+ * @Description: 全局使用的store名称
+ *
+ * 不要重复，也不要偷懒直接写字符串
+ */
+
+export const enum StoreNames {
+  Toast = 'Toast',
+}
