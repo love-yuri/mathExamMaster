d6faf1830189923d3d7c9568955842c659962ccf
love-yuri <yuri2078170658@gmail.com>
2024-08-13 00:58:38 +0800

    feat: 添加API操作注解到BaseController

--- kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseController.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseController.kt
@@ -1,13 +1,15 @@
 package math.yl.love.common.mybatis
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Schema
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-
 
 abstract class BaseController<Entity: BaseEntity, Mapper: BaseMapper<Entity>> {
 
@@ -18,14 +20,18 @@ abstract class BaseController<Entity: BaseEntity, Mapper: BaseMapper<Entity>> {
     private lateinit var baseService: BaseService<Entity, Mapper>
 
     @PostMapping("create")
+    @Operation(summary = "创建")
     fun create(@RequestBody value: Entity) = baseService.create(value)
 
     @PostMapping("update")
+    @Operation(summary = "更新")
     fun update(@RequestBody value: Entity) = baseService.update(value)
 
     @PostMapping("delete/{id}")
-    fun delete(@PathVariable id: Long) = baseService.delete(id)
+    @Operation(summary = "根据id删除")
+    fun delete(@PathVariable @Schema(description = "主键id")  id: Long) = baseService.delete(id)
 
     @PostMapping("get/{id}")
+    @Operation(summary = "根据id获取")
     fun get(@PathVariable id: Long): Entity = baseService.getById(id)
 }
