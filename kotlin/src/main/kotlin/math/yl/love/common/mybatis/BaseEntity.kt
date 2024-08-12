7eaea982481e335c23fe5a2be93a7b1dc808aac3
love-yuri <yuri2078170658@gmail.com>
2024-08-13 00:58:44 +0800

    feat: 添加Swagger注解以增强BaseEntity类的可读性和可维护性

--- kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseEntity.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseEntity.kt
@@ -2,6 +2,7 @@ package math.yl.love.common.mybatis
 
 import com.baomidou.mybatisplus.annotation.*
 import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 /**
@@ -11,20 +12,25 @@ import java.time.LocalDateTime
  */
 open class BaseEntity {
     @TableId(type = IdType.ASSIGN_ID)
+    @Schema(description = "主键id")
     val id: Long? = null
 
     @TableLogic
     @TableField("deleted")
+    @Schema(description = "是否被删除")
     val deleted: Boolean = false
 
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "created_time", fill = FieldFill.INSERT)
+    @Schema(description = "创建时间")
     val createdTime: LocalDateTime? = null
 
     @TableField("created_by")
+    @Schema(description = "创建用户")
     val createdBy: Long? = null
 
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "updated_time", fill = FieldFill.INSERT_UPDATE)
+    @Schema(description = "更新时间")
     val updatedTime: LocalDateTime? = null
 }
