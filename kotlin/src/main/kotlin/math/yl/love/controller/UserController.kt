ec3ec7b81a91e81e8467e8c55bb6b7ea065ccda9
love-yuri <yuri2078170658@gmail.com>
2024-08-13 00:59:05 +0800

    feat(UserController): 添加Swagger注解，优化API文档

--- kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
@@ -1,6 +1,8 @@
 package math.yl.love.controller
 
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import math.yl.love.common.base.R
 import math.yl.love.common.mybatis.BaseController
 import math.yl.love.database.entity.entity.User
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "用户管理")
 class UserController(
     val authenticationManager: AuthenticationManager
 ): BaseController<User, UserMapper>() {
 
-    @ApiOperationSupport(author = "xiaoymin@foxmail.com")
     @PostMapping("/login")
+    @Operation(summary = "登陆")
+    @ApiOperationSupport(author = "love-yuri")
     fun login(@RequestBody user: User): R<User?> {
         log.info("yuri: 获取的参数")
         return R.success(User())
