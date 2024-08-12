7ad4dfa89806ff9d6b9f7f81020d540436f8553b
love-yuri <yuri2078170658@gmail.com>
2024-08-13 00:59:10 +0800

    feat: 添加用户角色和密码字段

--- kotlin/src/main/kotlin/math/yl/love/database/entity/entity/User.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/entity/User.kt
@@ -2,15 +2,17 @@ package math.yl.love.database.entity.entity
 
 import com.baomidou.mybatisplus.annotation.TableField
 import com.baomidou.mybatisplus.annotation.TableName
+import io.swagger.v3.oas.annotations.media.Schema
 import math.yl.love.common.mybatis.BaseEntity
 
 @TableName("user")
 class User: BaseEntity() {
 
-    
+    @Schema(description = "用户角色")
     @TableField("role")
     val role: Int? = null
 
     @TableField("pass_word")
+    @Schema(description = "密码")
     val passWord: String? = null
 }
