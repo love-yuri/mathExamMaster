67f71a283b7b20ea1e39745d8780d9c3dc7a4aa8
love-yuri <yuri2078170658@gmail.com>
2024-08-13 00:58:51 +0800

    feat(JsonUtils): 添加对序列化注解的支持

--- kotlin/src/main/kotlin/math/yl/love/common/utils/JsonUtils.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/utils/JsonUtils.kt
@@ -16,12 +16,14 @@ object JsonUtils {
     }
 
     /**
-     * 任何数据转为json
+     * 将任何数据转为json
+     * 前提是需要添加 @Serializable 注解
      */
     inline fun <reified T> T.toJson() = json.encodeToString(this)
 
     /**
      * 解析字符串成json
+     * 待解析的类需要添加注解
      */
     inline fun <reified T> String.parseJson() = json.decodeFromString<T>(this)
 }
