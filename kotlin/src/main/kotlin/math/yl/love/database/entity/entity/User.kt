10cde0fe38fceb3f425ba4a1b9f12a150f6ad580
love-yuri <yuri2078170658@gmail.com>
2024-08-20 00:55:26 +0800

    feat: 添加User实体类的序列化注解

--- kotlin/src/main/kotlin/math/yl/love/database/entity/entity/User.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/entity/User.kt
@@ -3,9 +3,11 @@ package math.yl.love.database.entity.entity
 import com.baomidou.mybatisplus.annotation.TableField
 import com.baomidou.mybatisplus.annotation.TableName
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Serializable
 import math.yl.love.common.mybatis.BaseEntity
 
 @TableName("user")
+@Serializable
 class User: BaseEntity() {
 
     @Schema(description = "用户角色")
