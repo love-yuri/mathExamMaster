6c0c447136f2a893e4f4720634f076f8086c15e3
love-yuri <yuri2078170658@gmail.com>
2024-08-20 00:55:32 +0800

    feat(UserControllerQuery): 添加用户登录接口请求query参数描述

--- kotlin/src/main/kotlin/math/yl/love/database/entity/query/UserControllerQuery.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/query/UserControllerQuery.kt
@@ -1,6 +1,12 @@
 package math.yl.love.database.entity.query
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+/**
+ * 接口请求 query
+ */
+
 data class LoginQuery(
-    val id: Long,
-    val password: String
+    @Schema(description = "用户名") val id: Long,
+    @Schema(description = "密码") val password: String
 )
