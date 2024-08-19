8cee36275c438824c64d94fc59af470f0a3fc987
love-yuri <yuri2078170658@gmail.com>
2024-08-20 00:55:36 +0800

    feat(UserService): 添加登录功能

--- kotlin/src/main/kotlin/math/yl/love/database/service/UserService.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/service/UserService.kt
@@ -3,14 +3,21 @@ package math.yl.love.database.service
 import com.baomidou.mybatisplus.extension.kotlin.KtQueryWrapper
 import math.yl.love.common.mybatis.BaseEntity
 import math.yl.love.common.mybatis.BaseService
+import math.yl.love.common.utils.JsonUtils.toJson
+import math.yl.love.configuration.auth.DetailUserInfo
 import math.yl.love.database.entity.entity.User
+import math.yl.love.database.entity.query.LoginQuery
 import math.yl.love.database.mapper.UserMapper
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional(readOnly = true)
-class UserService : BaseService<User, UserMapper>() {
+class UserService(
+    private val authenticationManager: AuthenticationManager
+) : BaseService<User, UserMapper>() {
 
     override val queryWrapper: KtQueryWrapper<User> get() = KtQueryWrapper(User())
 
@@ -21,5 +28,13 @@ class UserService : BaseService<User, UserMapper>() {
 
         logger.info("yuri: 获取结果: $all")
         logger.info("yuri: 结果 -> ${list(queryWrapper.eq(BaseEntity::id, 1))}")
+    }
+
+    fun login(loginQuery: LoginQuery): User? {
+        logger.info("yuri: 参数$loginQuery")
+        val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(loginQuery.id, loginQuery.password)
+        val res = authenticationManager.authenticate(usernamePasswordAuthenticationToken)
+        logger.info("yuri: res: -> ${(res.principal as DetailUserInfo).toJson()} ")
+        return null
     }
 }
