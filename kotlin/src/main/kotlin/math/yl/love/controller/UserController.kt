073febb8900d02059cea12083909a94d43c1ce92
love-yuri <yuri2078170658@gmail.com>
2024-08-20 00:55:22 +0800

    feat(UserController): 修改登陆接口，使用LoginQuery服务进行登陆操作

--- kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
@@ -6,7 +6,9 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import math.yl.love.common.base.R
 import math.yl.love.common.mybatis.BaseController
 import math.yl.love.database.entity.entity.User
+import math.yl.love.database.entity.query.LoginQuery
 import math.yl.love.database.mapper.UserMapper
+import math.yl.love.database.service.UserService
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,15 +18,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/user")
 @Tag(name = "用户管理")
-class UserController(
-    val authenticationManager: AuthenticationManager
-): BaseController<User, UserMapper>() {
+class UserController: BaseController<User, UserMapper, UserService>() {
 
     @PostMapping("/login")
     @Operation(summary = "登陆")
-    @ApiOperationSupport(author = "love-yuri")
-    fun login(@RequestBody user: User): R<User?> {
-        log.info("yuri: 获取的参数")
-        return R.success(User())
+    fun login(@RequestBody query: LoginQuery): R<User?> {
+        return R.success(baseService.login(query))
     }
 }
