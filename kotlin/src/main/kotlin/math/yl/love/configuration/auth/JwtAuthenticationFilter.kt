7611d0c419237f54bff97436f8a9675f8918001d
love-yuri <yuri2078170658@gmail.com>
2024-08-21 00:56:55 +0800

    feat: 重构JwtAuthenticationFilter以增强安全性

--- kotlin/src/main/kotlin/math/yl/love/configuration/auth/JwtAuthenticationFilter.kt
+++ kotlin/src/main/kotlin/math/yl/love/configuration/auth/JwtAuthenticationFilter.kt
@@ -1,9 +1,17 @@
 package math.yl.love.configuration.auth
 
+import com.auth0.jwt.exceptions.JWTVerificationException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import math.yl.love.common.base.R
+import math.yl.love.common.base.SystemCode
+import math.yl.love.common.constant.HeadersConstant
+import math.yl.love.common.utils.JsonUtils.toJson
 import math.yl.love.common.utils.JwtUtils
+import math.yl.love.database.entity.entity.User
+import math.yl.love.database.entity.query.user.LoginQuery
+import math.yl.love.database.entity.result.user.LoginJwtResult
 import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -29,30 +37,28 @@ class JwtAuthenticationFilter : OncePerRequestFilter() {
     )
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-
-        log.info("uri: ${request.requestURI}")
-
         // 判断当前请求路径是否在排除的路径列表中
         if (safeUrl.any { request.requestURI.startsWith(it) }) {
             filterChain.doFilter(request, response) // 放行请求
             return
         }
 
-        getTokenFromRequest(request)?.also {
-            val username = JwtUtils.verifyTokenTest(it)
-            log.info("yuri: user: $username")
-            val authentication = UsernamePasswordAuthenticationToken(username, null, emptyList())
-            authentication.details = WebAuthenticationDetailsSource().buildDetails(request)
-            SecurityContextHolder.getContext().authentication = authentication
-
+        request.getHeader(HeadersConstant.AUTHORIZATION)?.also {
+            try {
+                val res = JwtUtils.verifyTokenAndParse(it, LoginJwtResult::class)
+                val authentication = UsernamePasswordAuthenticationToken(res!!.id, null, emptyList())
+                authentication.details = WebAuthenticationDetailsSource().buildDetails(request)
+                SecurityContextHolder.getContext().authentication = authentication
+                filterChain.doFilter(request, response)
+            } catch (e: JWTVerificationException) {
+                /* token 解析失败 或 过期 */
+                R.fail(response, SystemCode.AccessTokenError)
+            } catch (e: Exception) {
+                // 捕获其他异常
+                R.fail(response, SystemCode.UNAUTHORIZED)
+            }
+        } ?: run {
+            R.fail(response, SystemCode.UNAUTHORIZED)
         }
-        filterChain.doFilter(request, response)
-    }
-
-    private fun getTokenFromRequest(request: HttpServletRequest): String? {
-        val bearerToken = request.getHeader("Authorization")
-        return if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            bearerToken.substring(7)
-        } else null
     }
 }
