7a32253ba86e4a53ef7446bc46ca0557582ac777
love-yuri <yuri2078170658@gmail.com>
2024-08-21 00:57:37 +0800

    feat(UserController): 修改登陆接口返回LoginResult实体

--- kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/UserController.kt
@@ -1,15 +1,14 @@
 package math.yl.love.controller
 
-import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import math.yl.love.common.base.R
 import math.yl.love.common.mybatis.BaseController
 import math.yl.love.database.entity.entity.User
-import math.yl.love.database.entity.query.LoginQuery
+import math.yl.love.database.entity.query.user.LoginQuery
+import math.yl.love.database.entity.result.user.LoginResult
 import math.yl.love.database.mapper.UserMapper
 import math.yl.love.database.service.UserService
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,7 +21,7 @@ class UserController: BaseController<User, UserMapper, UserService>() {
 
     @PostMapping("/login")
     @Operation(summary = "登陆")
-    fun login(@RequestBody query: LoginQuery): R<User?> {
+    fun login(@RequestBody query: LoginQuery): R<LoginResult> {
         return R.success(baseService.login(query))
     }
 }
