d060c3affe59b66bea5f1eb784954de54c4912bd
love-yuri <yuri2078170658@gmail.com>
2024-08-21 00:56:26 +0800

    feat: 添加 BaseEntity 类并引入相关注解

--- kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseEntity.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseEntity.kt
@@ -3,6 +3,8 @@ package math.yl.love.common.mybatis
 import com.baomidou.mybatisplus.annotation.*
 import com.fasterxml.jackson.annotation.JsonFormat
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 
 /**
@@ -10,6 +12,7 @@ import java.time.LocalDateTime
  * id 为自动增长
  * 创建时间-更新时间会自动注入
  */
+@Serializable
 open class BaseEntity {
     @TableId(type = IdType.ASSIGN_ID)
     @Schema(description = "主键id")
@@ -23,6 +26,7 @@ open class BaseEntity {
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "created_time", fill = FieldFill.INSERT)
     @Schema(description = "创建时间")
+    @Contextual // 使用上下文序列化器
     val createdTime: LocalDateTime? = null
 
     @TableField("created_by")
@@ -32,5 +36,6 @@ open class BaseEntity {
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "updated_time", fill = FieldFill.INSERT_UPDATE)
     @Schema(description = "更新时间")
+    @Contextual // 使用上下文序列化器
     val updatedTime: LocalDateTime? = null
 }
