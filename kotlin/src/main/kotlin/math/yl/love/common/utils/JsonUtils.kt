1d305ce5d9e3726c665c48ea036b014d6e8e8382
love-yuri <yuri2078170658@gmail.com>
2024-08-21 00:56:31 +0800

    feat: 添加LocalDateTime序列化功能

--- kotlin/src/main/kotlin/math/yl/love/common/utils/JsonUtils.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/utils/JsonUtils.kt
@@ -2,8 +2,17 @@ package math.yl.love.common.utils
 
 
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encodeToString
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object JsonUtils {
     /**
@@ -13,6 +22,13 @@ object JsonUtils {
     val json = Json {
         encodeDefaults = true  // 启用序列化默认值
         explicitNulls = true // 反序列化null值
+
+        /**
+         * 序列化配置
+         */
+        serializersModule = SerializersModule {
+            contextual(LocalDateTime::class, LocalDateTimeSerializer)
+        }
     }
 
     /**
@@ -26,4 +42,23 @@ object JsonUtils {
      * 待解析的类需要添加注解
      */
     inline fun <reified T> String.parseJson() = json.decodeFromString<T>(this)
+
+
+    /**
+     * LocalData 时间序列化
+     */
+    object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+        private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+        override fun serialize(encoder: Encoder, value: LocalDateTime) {
+            encoder.encodeString(value.format(formatter))
+        }
+
+        override fun deserialize(decoder: Decoder): LocalDateTime {
+            return LocalDateTime.parse(decoder.decodeString(), formatter)
+        }
+    }
+
 }
