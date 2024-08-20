15f059573eafb2adb4a3c047cd63b53868ed6d55
love-yuri <yuri2078170658@gmail.com>
2024-08-21 00:56:06 +0800

    feat(AuthenticationEntryPointHandler): 删除身份验证入口处理器 refactor(LoginQuery): 重构登陆接口请求query

--- kotlin/src/main/kotlin/math/yl/love/database/entity/query/user/LoginQuery.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/query/user/LoginQuery.kt
@@ -1,11 +1,10 @@
-package math.yl.love.database.entity.query
+package math.yl.love.database.entity.query.user
 
 import io.swagger.v3.oas.annotations.media.Schema
 
 /**
- * 接口请求 query
+ * 登陆接口请求 query
  */
-
 data class LoginQuery(
     @Schema(description = "用户名") val id: Long,
     @Schema(description = "密码") val password: String
