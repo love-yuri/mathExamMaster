72ba09254261b7f7a12db9b4fc0d18eaf92528aa
love-yuri <yuri2078170658@gmail.com>
2024-08-23 00:23:56 +0800

    feat(config): 添加JsonConfig类以配置全局json反序列化

---
+++ kotlin/src/main/kotlin/math/yl/love/configuration/config/JsonConfig.kt
@@ -0,0 +1,71 @@
+package math.yl.love.configuration.config
+
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer
+import com.baomidou.mybatisplus.core.MybatisConfiguration
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import org.apache.ibatis.type.LocalDateTimeTypeHandler
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+@Configuration
+@Import(JacksonAutoConfiguration::class)
+class JsonConfig : ConfigurationCustomizer {
+
+    companion object {
+        /**
+         * 全局json反序列化配置
+         */
+        @OptIn(ExperimentalSerializationApi::class)
+        val json = Json {
+            encodeDefaults = true  // 启用序列化默认值
+            explicitNulls = true // 反序列化null值
+            serializersModule = SerializersModule {
+                contextual(LocalDateTime::class, LocalDateTimeSerializer)
+            }
+        }
+    }
+
+    override fun customize(configuration: MybatisConfiguration?) {
+        configuration?.typeHandlerRegistry?.register(LocalDateTimeTypeHandler::class.java)
+    }
+
+    object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+        private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+        override fun serialize(encoder: Encoder, value: LocalDateTime) {
+            encoder.encodeString(value.format(formatter))
+        }
+
+        override fun deserialize(decoder: Decoder): LocalDateTime {
+            return LocalDateTime.parse(decoder.decodeString(), formatter)
+        }
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            registerModule(JavaTimeModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        }
+    }
+
+}
