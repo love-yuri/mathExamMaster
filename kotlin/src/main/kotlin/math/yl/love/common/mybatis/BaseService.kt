12de74cc84eebee67f67a679a9c7a7955b54eb2c
love-yuri <yuri2078170658@gmail.com>
2024-08-23 00:23:40 +0800

    feat: 修改BaseService类以支持CRUD操作

--- kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseService.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseService.kt
@@ -1,41 +1,47 @@
 package math.yl.love.common.mybatis
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
-import com.baomidou.mybatisplus.extension.kotlin.KtQueryWrapper
-import com.baomidou.mybatisplus.extension.service.IService
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 
-
-abstract class BaseService <Entity: BaseEntity, Mapper: BaseMapper<Entity>> : ServiceImpl<Mapper, Entity>(), IService<Entity> {
-
-    protected val log: Logger = LoggerFactory.getLogger(this::class.java)
-
-    /**
-     * 快捷lambda Query查询类
-     */
-    protected abstract val queryWrapper: KtQueryWrapper<Entity>
+@Component
+abstract class BaseService <Entity: BaseEntity, Mapper: BaseMapper<Entity>> {
+    protected val log: Logger = LoggerFactory.getLogger(javaClass)
 
     /**
-     * 日志类
-     * 因为和本地的log冲突，所以这里改用logger
+     * 基础Mapper类
      */
-    protected val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    @Autowired
+    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+    protected lateinit var baseMapper: Mapper
 
     /**
      * 创建
      */
-    fun create(entity: Entity) = baseMapper.insert(entity)
+    fun create(entity: Entity) = baseMapper.insert(entity) == 1
 
     /**
      * 更新
      */
-    fun update(entity: Entity) = baseMapper.updateById(entity)
+    fun update(entity: Entity) = baseMapper.updateById(entity) == 1
 
     /**
      * 删除
      */
-    fun delete(id: Long) = baseMapper.deleteById(id)
+    fun delete(id: Long) = baseMapper.deleteById(id) == 1
+
+    /**
+     * 获取所有返回结果
+     */
+    fun list(): List<Entity> = baseMapper.selectList(QueryWrapper())
+
+    /**
+     * 通过id查询结果
+     * 如果id为null则直接返回null
+     */
+    fun getById(id: Long?): Entity? = id?.let { baseMapper.selectById(it) }
 }
 
