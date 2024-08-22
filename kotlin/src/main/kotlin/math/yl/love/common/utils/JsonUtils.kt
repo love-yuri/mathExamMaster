2c1008652550f211f580d899c741eff2c4782b22
love-yuri <yuri2078170658@gmail.com>
2024-08-23 00:23:45 +0800

    feat: 重构JsonUtils类以优化代码结构

--- kotlin/src/main/kotlin/math/yl/love/common/utils/JsonUtils.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/utils/JsonUtils.kt
@@ -11,25 +11,13 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
+import math.yl.love.configuration.config.JsonConfig
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 object JsonUtils {
-    /**
-     * 全局json反序列化配置
-     */
-    @OptIn(ExperimentalSerializationApi::class)
-    val json = Json {
-        encodeDefaults = true  // 启用序列化默认值
-        explicitNulls = true // 反序列化null值
 
-        /**
-         * 序列化配置
-         */
-        serializersModule = SerializersModule {
-            contextual(LocalDateTime::class, LocalDateTimeSerializer)
-        }
-    }
+    val json: Json get() = JsonConfig.json
 
     /**
      * 将任何数据转为json
@@ -42,23 +30,4 @@ object JsonUtils {
      * 待解析的类需要添加注解
      */
     inline fun <reified T> String.parseJson() = json.decodeFromString<T>(this)
-
-
-    /**
-     * LocalData 时间序列化
-     */
-    object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
-        private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
-
-        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
-
-        override fun serialize(encoder: Encoder, value: LocalDateTime) {
-            encoder.encodeString(value.format(formatter))
-        }
-
-        override fun deserialize(decoder: Decoder): LocalDateTime {
-            return LocalDateTime.parse(decoder.decodeString(), formatter)
-        }
-    }
-
 }
