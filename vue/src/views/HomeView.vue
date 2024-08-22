2cd62ec374d0a093bcecb68ca508df51e82471d0
love-yuri <yuri2078170658@gmail.com>
2024-08-23 00:25:45 +0800

    feat(HomeView.vue): 移除用户API调用

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-08-10 16:02:45
- * @LastEditTime: 2024-08-21 00:17:15
+ * @LastEditTime: 2024-08-22 23:49:11
  * @Description: 主页面
 -->
 <template>
@@ -13,19 +13,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { Button } from '@/components';
-import { userApi } from '@/api/userApi';
 import { testApi } from '@/api/testApi';
 // import message from '@/common/utils/message';
 
 const baseUrl = ref(import.meta.env.VITE_BASE_URL);
 
-async function showv() {
-  const res = await userApi.login({
-    id: 1,
-    password: 'yuri',
-  });
-  console.log('res', res);
-}
+async function showv() {}
 
 async function parse() {
   await testApi.hello();
