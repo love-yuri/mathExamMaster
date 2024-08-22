c597f9b20fc2772716738800aa0c60bd832b0cb3
love-yuri <yuri2078170658@gmail.com>
2024-08-23 00:23:26 +0800

    feat: 更新BaseController类以返回R对象

--- kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseController.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseController.kt
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Schema
+import math.yl.love.common.base.R
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,10 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
-@RequestMapping("/user")
 abstract class BaseController<Entity: BaseEntity, Mapper: BaseMapper<Entity>, Service: BaseService<Entity, Mapper>> {
 
-    protected val logger: Logger = LoggerFactory.getLogger(javaClass)
+    protected val log: Logger = LoggerFactory.getLogger(javaClass)
 
     @Autowired
     @Suppress("SpringJavaInjectionPointsAutowiringInspection")
@@ -23,17 +23,17 @@ abstract class BaseController<Entity: BaseEntity, Mapper: BaseMapper<Entity>, Se
 
     @PostMapping("create")
     @Operation(summary = "创建")
-    fun create(@RequestBody value: Entity) = baseService.create(value)
+    fun create(@RequestBody value: Entity) = R.success(baseService.create(value))
 
     @PostMapping("update")
     @Operation(summary = "更新")
-    fun update(@RequestBody value: Entity) = baseService.update(value)
+    fun update(@RequestBody value: Entity) = R.success(baseService.update(value))
 
     @PostMapping("delete/{id}")
     @Operation(summary = "根据id删除")
-    fun delete(@PathVariable @Schema(description = "主键id")  id: Long) = baseService.delete(id)
+    fun delete(@PathVariable @Schema(description = "主键id") id: Long) = R.success(baseService.delete(id))
 
     @PostMapping("get/{id}")
     @Operation(summary = "根据id获取")
-    fun get(@PathVariable id: Long): Entity = baseService.getById(id)
+    fun get(@PathVariable id: Long) = R.success(baseService.getById(id))
 }
