59485e6b9933add79478e18e22b89427d1967a44
love-yuri <yuri2078170658@gmail.com>
2024-08-23 00:24:57 +0800

    feat: 修改用户登录API参数类型

--- vue/src/api/userApi.ts
+++ vue/src/api/userApi.ts
@@ -7,7 +7,7 @@ import { BaseApi, RequestType } from '@/common/base/baseApi';
 class UserApi extends BaseApi {
   readonly baseUrl: string = '/user';
 
-  login = (params: { id: number; password: string }) => {
+  login = (params: { id: number | string; password: string }) => {
     return this.add(RequestType.POST, 'login', params);
   };
 }
