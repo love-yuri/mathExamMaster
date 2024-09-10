53cf0824db9c0bc557d55656e481eedba09d16c6
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:48:59 +0800

    feat(BaseEntity): 重命名BaseEntity类并更新注解

--- kotlin/src/main/kotlin/math/yl/love/database/entity/entity/BaseEntity.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/entity/BaseEntity.kt
@@ -1,10 +1,9 @@
-package math.yl.love.common.mybatis
+package math.yl.love.database.entity.entity
 
 import com.baomidou.mybatisplus.annotation.*
-import com.fasterxml.jackson.annotation.JsonFormat
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import math.yl.love.configuration.config.JsonConfig
 import java.time.LocalDateTime
 
 /**
@@ -13,27 +12,27 @@ import java.time.LocalDateTime
  * 创建时间-更新时间会自动注入
  */
 @Serializable
-open class BaseEntity {
+open class BaseEntity(
     @TableId(type = IdType.ASSIGN_ID)
     @Schema(description = "主键id")
-    val id: Long? = null
+    val id: Long? = null,
 
     @TableLogic
     @TableField("deleted")
     @Schema(description = "是否被删除")
-    val deleted: Boolean = false
+    val deleted: Boolean = false,
 
     @TableField(value = "created_time", fill = FieldFill.INSERT)
     @Schema(description = "创建时间")
-    @Serializable(with = JsonConfig.LocalDateTimeSerializer::class)
-    val createdTime: LocalDateTime? = null
+    @Contextual
+    val createdTime: LocalDateTime? = null,
 
     @TableField("created_by")
     @Schema(description = "创建用户")
-    val createdBy: Long? = null
+    val createdBy: Long? = null,
 
     @TableField(value = "updated_time", fill = FieldFill.INSERT_UPDATE)
     @Schema(description = "更新时间")
-    @Serializable(with = JsonConfig.LocalDateTimeSerializer::class)
-    val updatedTime: LocalDateTime? = null
-}
+    @Contextual
+    val updatedTime: LocalDateTime? = null,
+)
