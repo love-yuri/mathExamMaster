c7d141a4ebec310fef6ee7cb3e222206885470d2
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:51:01 +0800

    feat(LoginResult): 添加序列化功能

--- kotlin/src/main/kotlin/math/yl/love/database/entity/result/user/LoginResult.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/result/user/LoginResult.kt
@@ -1,8 +1,10 @@
 package math.yl.love.database.entity.result.user
 
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Serializable
 import math.yl.love.database.entity.entity.User
 
+@Serializable
 data class LoginResult(
     @Schema(description = "用户信息")
     val user: User,
