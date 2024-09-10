d1e97694dfef678e81a8c3257220d34cc9c73f20
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:50:57 +0800

    feat(LoginJwtResult): 更改用户登录JWT结果实体类

--- kotlin/src/main/kotlin/math/yl/love/database/entity/result/user/LoginJwtResult.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/result/user/LoginJwtResult.kt
@@ -1,10 +1,12 @@
 package math.yl.love.database.entity.result.user
 
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class LoginJwtResult (
-    @Schema(description = "用户id")
-    val id: Long,
+    @Schema(description = "用户名")
+    val username: String,
 
     @Schema(description = "角色")
     val role: Int?
