5dff759d0cf07d530236588701aaab759fee2a21
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:50:52 +0800

    feat: 新增 ColumnInfo 数据类

---
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/result/system/ColumnInfo.kt
@@ -0,0 +1,10 @@
+package math.yl.love.database.entity.result.system
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ColumnInfo(
+    val field: String,
+    val type: String,
+    val comment: String? = null,
+)
