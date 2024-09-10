c5b0e13d71802e136670122ce3bac593ecf04bc8
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:50:10 +0800

    feat(Knife4jConfig): 添加了对Swagger全局标签的支持

--- kotlin/src/main/kotlin/math/yl/love/configuration/config/Knife4jConfig.kt
+++ kotlin/src/main/kotlin/math/yl/love/configuration/config/Knife4jConfig.kt
@@ -1,5 +1,6 @@
 package math.yl.love.configuration.config
 
+import io.swagger.v3.core.util.AnnotationsUtils.getTags
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.Operation
 import io.swagger.v3.oas.models.info.Info
@@ -10,15 +11,19 @@ import math.yl.love.common.constant.HeadersConstant
 import org.springdoc.core.customizers.GlobalOperationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.method.HandlerMethod
 
 
 @Configuration
 class Knife4jConfig {
+
     @Bean
     fun customOpenAPI(): OpenAPI {
         return OpenAPI().info(
-            Info().title("API Documentation").version("1.0").description("API Documentation with global header")
+            Info().title("API Documentation")
+                .version("1.0")
+                .description("API Documentation with global header")
         ).addServersItem(Server().url("/"))
     }
 
@@ -32,4 +37,6 @@ class Knife4jConfig {
             operation
         }
     }
+
+
 }
