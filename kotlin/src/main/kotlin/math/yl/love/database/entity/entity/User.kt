f4fbffd4af2edac497a42d74eba52e0bdd206556
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:50:43 +0800

    feat: 更新用户实体类定义

--- kotlin/src/main/kotlin/math/yl/love/database/entity/entity/User.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/entity/User.kt
@@ -1,20 +1,50 @@
 package math.yl.love.database.entity.entity
 
-import com.baomidou.mybatisplus.annotation.TableField
-import com.baomidou.mybatisplus.annotation.TableName
+import com.baomidou.mybatisplus.annotation.*
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import math.yl.love.common.mybatis.BaseEntity
+import kotlinx.serialization.Transient
+import java.time.LocalDateTime
 
 @TableName("user")
 @Serializable
-class User: BaseEntity() {
+data class User (
+    @TableId(type = IdType.ASSIGN_ID)
+    @Schema(description = "主键id")
+    override val id: Long? = null,
+
+    @Transient
+    @TableLogic
+    @TableField("deleted")
+    @Schema(description = "是否被删除")
+    override val deleted: Boolean = false,
+
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    @Schema(description = "创建时间")
+    @Contextual
+    override val createTime: LocalDateTime = LocalDateTime.now(),
+
+    @TableField("create_by")
+    @Schema(description = "创建用户")
+    override val createBy: Long = 0,
+
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
+    @Schema(description = "更新时间")
+    @Contextual
+    override val updateTime: LocalDateTime = LocalDateTime.now(),
 
     @Schema(description = "用户角色")
     @TableField("role")
-    val role: Int? = null
+    val role: Int? = null,
+
+    @TableField("user_name")
+    @Schema(description = "用户名")
+    val username: String? = null,
 
     @TableField("pass_word")
+    @Transient
     @Schema(description = "密码")
-    val passWord: String? = null
-}
+    val password: String? = null,
+
+) : BaseEntity
