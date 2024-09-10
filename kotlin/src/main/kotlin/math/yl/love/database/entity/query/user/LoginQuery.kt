e04881dc282eece5e135c35b56e3ffe95dc0904c
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:50:48 +0800

    feat(LoginQuery): 更新登陆接口请求参数

--- kotlin/src/main/kotlin/math/yl/love/database/entity/query/user/LoginQuery.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/query/user/LoginQuery.kt
@@ -1,11 +1,13 @@
 package math.yl.love.database.entity.query.user
 
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Serializable
 
 /**
  * 登陆接口请求 query
  */
+@Serializable
 data class LoginQuery(
-    @Schema(description = "用户名") val id: Long,
-    @Schema(description = "密码") val password: String
+    @Schema(description = "用户名") val username: String,
+    @Schema(description = "密码") val password: String,
 )
