c333b1b8322b04bdb54d63b331a7ee3c5f225aa2
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:50:24 +0800

    feat: 创建系统接口控制器

---
+++ kotlin/src/main/kotlin/math/yl/love/controller/SystemController.kt
@@ -0,0 +1,21 @@
+package math.yl.love.controller
+
+import io.swagger.v3.oas.annotations.tags.Tag
+import math.yl.love.common.base.R
+import math.yl.love.database.service.SystemService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/system")
+@Tag(name = "系统接口")
+class SystemController(
+    private val systemService: SystemService
+) {
+    @PostMapping("generate")
+    fun generate(): R<String> {
+        return R.success(systemService.generate())
+    }
+}
