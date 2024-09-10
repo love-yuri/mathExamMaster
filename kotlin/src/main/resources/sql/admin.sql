d7ad6bb3b3902e1dd97419c5d6fe533e3d5c7e50
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:51:31 +0800

    feat: 添加用户表admin.sql，包含用户角色、账号、密码、创建时间和创建人等信息。

--- kotlin/src/main/resources/sql/admin.sql
+++ kotlin/src/main/resources/sql/admin.sql
@@ -2,11 +2,38 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE user (
   `id` bigint NOT NULL COMMENT 'id',
   `role` tinyint(1) NOT NULL default 1 comment '角色: 1: 管理员, 2: 老师， 3: 学生',
+  `user_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '账号',
   `pass_word` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci comment '密码',
   `deleted` boolean NOT NULL DEFAULT FALSE comment '是否删除',
-  `created_time` datetime NOT NULL COMMENT '创建时间',
-  `created_by` bigint NULL DEFAULT NULL COMMENT '创建人',
-  `updated_time` datetime NOT NULL COMMENT '最后修改时间',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `create_by` bigint NULL DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime NOT NULL COMMENT '最后修改时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户表';
 
+insert into user value (1, 1, 'yuri', '$2a$10$b2nCP59gVbx1Eqhw6c2p2OS6jlbX.8GrjziRuGNjTQ.1lKuU5y3f.', 0, NOW(), 1, NOW());
+
+SELECT
+    TABLE_COMMENT
+FROM
+    INFORMATION_SCHEMA.TABLES
+WHERE
+    TABLE_SCHEMA = 'zyl'
+  AND TABLE_NAME = 'user';
+
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'zyl' AND TABLE_NAME = 'user';
+
+SELECT
+    COLUMN_NAME AS `field`,
+    COLUMN_TYPE AS `type`,
+    COLUMN_COMMENT AS `comment`,
+    TABLE_COMMENT AS `table_comment`
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+    left join INFORMATION_SCHEMA.TABLES on INFORMATION_SCHEMA.COLUMNS.TABLE_NAME =  INFORMATION_SCHEMA.TABLES.TABLE_NAME
+WHERE
+    INFORMATION_SCHEMA.COLUMNS.TABLE_SCHEMA = 'zyl'
+  AND INFORMATION_SCHEMA.COLUMNS.TABLE_NAME = 'user';
+
+
+select * from user;
