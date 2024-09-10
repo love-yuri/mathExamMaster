0a2c707d44cde4c419ece6a43ae75a5cbc2d4ea8
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:49:26 +0800

    feat(BaseController): 移除了ApiOperationSupport导入，添加了BaseEntity导入

--- kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseController.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/BaseController.kt
@@ -1,17 +1,16 @@
 package math.yl.love.common.mybatis
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
-import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Schema
 import math.yl.love.common.base.R
+import math.yl.love.database.entity.entity.BaseEntity
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 
 abstract class BaseController<Entity: BaseEntity, Mapper: BaseMapper<Entity>, Service: BaseService<Entity, Mapper>> {
 
