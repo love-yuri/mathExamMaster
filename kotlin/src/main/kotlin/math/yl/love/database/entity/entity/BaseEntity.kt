c4f1b81d957a65e78b07a6a820a76e1eb614ecd2
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:50:39 +0800

    feat: 重构基础实体类以使用接口

--- kotlin/src/main/kotlin/math/yl/love/database/entity/entity/BaseEntity.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/entity/entity/BaseEntity.kt
@@ -11,28 +11,10 @@ import java.time.LocalDateTime
  * id 为自动增长
  * 创建时间-更新时间会自动注入
  */
-@Serializable
-open class BaseEntity(
-    @TableId(type = IdType.ASSIGN_ID)
-    @Schema(description = "主键id")
-    val id: Long? = null,
-
-    @TableLogic
-    @TableField("deleted")
-    @Schema(description = "是否被删除")
-    val deleted: Boolean = false,
-
-    @TableField(value = "created_time", fill = FieldFill.INSERT)
-    @Schema(description = "创建时间")
-    @Contextual
-    val createdTime: LocalDateTime? = null,
-
-    @TableField("created_by")
-    @Schema(description = "创建用户")
-    val createdBy: Long? = null,
-
-    @TableField(value = "updated_time", fill = FieldFill.INSERT_UPDATE)
-    @Schema(description = "更新时间")
-    @Contextual
-    val updatedTime: LocalDateTime? = null,
-)
+sealed interface BaseEntity {
+    val id: Long?
+    val deleted: Boolean
+    val createTime: LocalDateTime
+    val createBy: Long
+    val updateTime: LocalDateTime
+}
