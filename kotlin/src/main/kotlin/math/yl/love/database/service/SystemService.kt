952a392e80e08d56078ae5d324b3137d5424e94d
love-yuri <yuri2078170658@gmail.com>
2024-09-13 23:56:18 +0800

    feat(SystemService): 添加GenerateParam参数到generate函数

--- kotlin/src/main/kotlin/math/yl/love/database/service/SystemService.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/service/SystemService.kt
@@ -1,14 +1,16 @@
 package math.yl.love.database.service
 
+import math.yl.love.database.entity.query.system.params.GenerateParam
 import math.yl.love.database.mapper.SystemMapper
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.RequestBody
 
 @Service
 @Transactional(readOnly = true)
 class SystemService(
     private val autoMysqlService: AutoMysqlService
 ) {
-    fun generate() = autoMysqlService.generate()
+    fun generate(param: GenerateParam) = autoMysqlService.generate(param)
 }
