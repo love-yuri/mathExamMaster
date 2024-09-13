2bd71706ac156c28aa409bc558a2ac63e8257a31
love-yuri <yuri2078170658@gmail.com>
2024-09-13 23:56:02 +0800

    feat: 修改AutoFillHandler类以处理新增字段

--- kotlin/src/main/kotlin/math/yl/love/common/mybatis/AutoFillHandler.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/AutoFillHandler.kt
@@ -1,7 +1,10 @@
 package math.yl.love.common.mybatis
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler
+import math.yl.love.common.base.Log.log
+import math.yl.love.database.entity.result.user.LoginJwtResult
 import org.apache.ibatis.reflection.MetaObject
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 
@@ -11,11 +14,21 @@ import java.time.LocalDateTime
 @Component
 class AutoFillHandler : MetaObjectHandler {
     override fun insertFill(metaObject: MetaObject) {
-        this.strictInsertFill(metaObject, "createdTime", LocalDateTime::now, LocalDateTime::class.java)
-        this.strictInsertFill(metaObject, "updatedTime", LocalDateTime::now, LocalDateTime::class.java)
+        metaObject.setterNames.forEach { fieldName ->
+            log.info("$fieldName = ${metaObject.getValue(fieldName)}")
+        }
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::class.java, LocalDateTime.now())
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime::class.java, LocalDateTime.now())
+        val authentication = SecurityContextHolder.getContext().authentication
+        val userDetails = authentication.principal as LoginJwtResult
+        this.strictInsertFill(metaObject, "createBy", String::class.java, userDetails.username)
+        this.strictInsertFill(metaObject, "updateBy", String::class.java, userDetails.username)
     }
 
     override fun updateFill(metaObject: MetaObject) {
-        this.strictUpdateFill(metaObject, "updatedTime", LocalDateTime::now, LocalDateTime::class.java)
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime::class.java)
+        val authentication = SecurityContextHolder.getContext().authentication
+        val userDetails = authentication.principal as LoginJwtResult
+        this.strictInsertFill(metaObject, "updateBy", String::class.java, userDetails.username)
     }
 }
