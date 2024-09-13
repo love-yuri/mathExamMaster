f7caa8466d7e1ddb7bbe7cb926a6d70ba591824e
love-yuri <yuri2078170658@gmail.com>
2024-09-13 23:56:43 +0800

    feat(SystemController): 添加generate方法请求体参数

--- kotlin/src/main/kotlin/math/yl/love/controller/SystemController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/SystemController.kt
@@ -2,9 +2,11 @@ package math.yl.love.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
 import math.yl.love.common.base.R
+import math.yl.love.database.entity.query.system.params.GenerateParam
 import math.yl.love.database.service.SystemService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,7 +17,5 @@ class SystemController(
     private val systemService: SystemService
 ) {
     @PostMapping("generate")
-    fun generate(): R<String> {
-        return R.success(systemService.generate())
-    }
+    fun generate(@RequestBody param: GenerateParam) = R.success(systemService.generate(param))
 }
