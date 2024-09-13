f12ad25fff962d8409b81c814f12a413a0ac902f
love-yuri <yuri2078170658@gmail.com>
2024-09-13 23:56:37 +0800

    feat(JwtUtils): 修改默认过期时间配置为8天

--- kotlin/src/main/kotlin/math/yl/love/common/utils/JwtUtils.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/utils/JwtUtils.kt
@@ -39,7 +39,7 @@ object JwtUtils {
      * 默认过期时间配置
      * 默认是8h
      */
-    private const val DEFAULT_EXPIRES = 60 * 2
+    private const val DEFAULT_EXPIRES = 60 * 60 * 8 * 24
 
     /**
      * 创建jwtToken
