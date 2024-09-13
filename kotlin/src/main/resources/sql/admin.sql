537e615183b4275af0f6bb346cfcd2237dd7d18d
love-yuri <yuri2078170658@gmail.com>
2024-09-13 23:56:08 +0800

    feat(admin.sql): 更新用户表字段及查询语句

--- kotlin/src/main/resources/sql/admin.sql
+++ kotlin/src/main/resources/sql/admin.sql
@@ -1,17 +1,23 @@
+DROP DATABASE IF EXISTS zyl;
+CREATE DATABASE zyl CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+use zyl;
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE user (
   `id` bigint NOT NULL COMMENT 'id',
   `role` tinyint(1) NOT NULL default 1 comment '角色: 1: 管理员, 2: 老师， 3: 学生',
-  `user_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '账号',
+  `user_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '用户名',
   `pass_word` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci comment '密码',
   `deleted` boolean NOT NULL DEFAULT FALSE comment '是否删除',
   `create_time` datetime NOT NULL COMMENT '创建时间',
-  `create_by` bigint NULL DEFAULT NULL COMMENT '创建人',
+  `create_by` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '创建用户',
   `update_time` datetime NOT NULL COMMENT '最后修改时间',
+  `update_by` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '更新用户',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户表';
 
-insert into user value (1, 1, 'yuri', '$2a$10$b2nCP59gVbx1Eqhw6c2p2OS6jlbX.8GrjziRuGNjTQ.1lKuU5y3f.', 0, NOW(), 1, NOW());
+insert into user value (1, 1, 'yuri', '$2a$10$b2nCP59gVbx1Eqhw6c2p2OS6jlbX.8GrjziRuGNjTQ.1lKuU5y3f.', 0, NOW(), 1, NOW(), 1);
 
 SELECT
     TABLE_COMMENT
@@ -29,11 +35,10 @@ SELECT
     COLUMN_COMMENT AS `comment`,
     TABLE_COMMENT AS `table_comment`
 FROM
-    INFORMATION_SCHEMA.COLUMNS
-    left join INFORMATION_SCHEMA.TABLES on INFORMATION_SCHEMA.COLUMNS.TABLE_NAME =  INFORMATION_SCHEMA.TABLES.TABLE_NAME
+    INFORMATION_SCHEMA.COLUMNS as col
+    left join INFORMATION_SCHEMA.TABLES as tab on col.TABLE_NAME = tab.TABLE_NAME
 WHERE
-    INFORMATION_SCHEMA.COLUMNS.TABLE_SCHEMA = 'zyl'
-  AND INFORMATION_SCHEMA.COLUMNS.TABLE_NAME = 'user';
-
+    col.TABLE_SCHEMA = 'zyl' AND col.TABLE_NAME = 'user' AND
+    tab.TABLE_SCHEMA = 'zyl' AND tab.TABLE_NAME = 'user';
 
 select * from user;
