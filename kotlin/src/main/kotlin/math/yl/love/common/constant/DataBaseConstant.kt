dd6a6a55b477e523987368228f86809418147e29
love-yuri <yuri2078170658@gmail.com>
2024-09-19 22:12:00 +0800

    feat: 添加 DataBaseConstant 常量对象

---
+++ kotlin/src/main/kotlin/math/yl/love/common/constant/DataBaseConstant.kt
@@ -0,0 +1,12 @@
+package math.yl.love.common.constant
+
+object DataBaseConstant {
+    /**
+     * 常用字段
+     */
+    const val CREATE_BY = "create_by"
+    const val CREATE_TIME = "create_time"
+    const val UPDATE_BY = "update_by"
+    const val UPDATE_TIME = "update_time"
+    const val DELETED = "deleted"
+}
