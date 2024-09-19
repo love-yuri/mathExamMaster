aec0e0bf4b5c95c9ba1f0be0d485b95cba92d8db
love-yuri <yuri2078170658@gmail.com>
2024-09-19 22:12:22 +0800

    feat: 添加下划线命名转小驼峰命名方法

--- kotlin/src/main/kotlin/math/yl/love/common/utils/StrUtils.kt
+++ kotlin/src/main/kotlin/math/yl/love/common/utils/StrUtils.kt
@@ -7,6 +7,25 @@ import kotlin.String
  * 字符串工具箱
  */
 object StrUtils {
-
+    /**
+     * 将下划线命名转小驼峰命名
+     * @param lower 是否是小驼峰，默认返回小驼峰命名法
+     */
+    fun String.snakeToCamel(lower: Boolean = true): String {
+        return this.split("_")
+            .mapIndexed { index, part ->
+                if (index == 0) {
+                    if (lower) {
+                        part.lowercase()
+                    } else {
+                        part.lowercase().replaceFirstChar { it.uppercaseChar() }
+                    }
+                } else {
+                    // 其余部分首字母大写
+                    part.replaceFirstChar { it.uppercaseChar() }
+                }
+            }
+            .joinToString("")
+    }
 
 }
