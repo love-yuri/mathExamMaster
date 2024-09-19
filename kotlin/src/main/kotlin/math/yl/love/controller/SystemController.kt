89a1c5fd9202c93e23fd23926126df280961d6c6
love-yuri <yuri2078170658@gmail.com>
2024-09-19 22:12:42 +0800

    feat(SystemController): 更新SystemController.kt中的包导入路径

--- kotlin/src/main/kotlin/math/yl/love/controller/SystemController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/SystemController.kt
@@ -2,9 +2,8 @@ package math.yl.love.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
 import math.yl.love.common.base.R
-import math.yl.love.database.entity.query.system.params.GenerateParam
+import math.yl.love.database.domain.params.system.GenerateParam
 import math.yl.love.database.service.SystemService
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
