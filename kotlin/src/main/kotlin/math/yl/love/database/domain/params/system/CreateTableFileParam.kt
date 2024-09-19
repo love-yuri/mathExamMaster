e5552aa4dd4df672f0505058b589cb24c37cc577
love-yuri <yuri2078170658@gmail.com>
2024-09-19 22:13:07 +0800

    feat: 创建 CreateTableFileParam 数据类

---
+++ kotlin/src/main/kotlin/math/yl/love/database/domain/params/system/CreateTableFileParam.kt
@@ -0,0 +1,14 @@
+package math.yl.love.database.domain.params.system
+
+import math.yl.love.database.domain.result.system.ColumnInfoResult
+
+data class CreateTableFileParam(
+    val tableName: String,
+    val entityName: String,
+    val tableComment: String?,
+    var entity: String = "",
+    var mapper: String = "",
+    var service: String = "",
+    var controller: String = "",
+    val column: List<ColumnInfoResult>
+)
