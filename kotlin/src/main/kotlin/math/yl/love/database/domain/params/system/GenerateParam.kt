fe649462bbbe07be578741d2488446383cb034ac
love-yuri <yuri2078170658@gmail.com>
2024-09-19 22:13:12 +0800

    feat(GenerateParam.kt): 添加默认数据库名参数

--- kotlin/src/main/kotlin/math/yl/love/database/domain/params/system/GenerateParam.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/domain/params/system/GenerateParam.kt
@@ -5,5 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class GenerateParam (
     val tableName: String,
-    val dataBaseName: String,
+    val dataBaseName: String = "zyl",
 )
