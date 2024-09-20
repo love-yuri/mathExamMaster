8525d9a3a1c65c8c98257334e970baffb813d861
love-yuri <yuri2078170658@gmail.com>
2024-09-20 22:57:43 +0800

    feat: 在generate.sql中添加查询用户字段的SQL语句

---
+++ kotlin/src/main/resources/sql/generate.sql
@@ -0,0 +1,13 @@
+SELECT
+    COLUMN_NAME AS `field`,
+    COLUMN_TYPE AS `type`,
+    COLUMN_COMMENT AS `comment`,
+    TABLE_COMMENT AS `table_comment`,
+    COLUMN_DEFAULT AS `default_value`,
+    IS_NULLABLE AS is_nullable
+FROM
+    INFORMATION_SCHEMA.COLUMNS as col
+        left join INFORMATION_SCHEMA.TABLES as tab on col.TABLE_NAME = tab.TABLE_NAME
+WHERE
+    col.TABLE_SCHEMA = 'zyl' AND col.TABLE_NAME = 'user' AND
+    tab.TABLE_SCHEMA = 'zyl' AND tab.TABLE_NAME = 'user';
