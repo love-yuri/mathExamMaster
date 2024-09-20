a250575aa7971917e21b92d7490c77f946d39920
love-yuri <yuri2078170658@gmail.com>
2024-09-20 22:57:38 +0800

    feat: 修改用户表和文件列表表的字段属性

--- kotlin/src/main/resources/sql/admin.sql
+++ kotlin/src/main/resources/sql/admin.sql
@@ -3,42 +3,37 @@ CREATE DATABASE zyl CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 use zyl;
 
+# CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE user (
   `id` bigint NOT NULL COMMENT 'id',
   `role` tinyint(1) NOT NULL default 1 comment '角色: 1: 管理员, 2: 老师， 3: 学生',
-  `user_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '用户名',
-  `pass_word` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci comment '密码',
+  `user_name` varchar(16) NOT NULL unique comment '用户名',
+  `pass_word` varchar(128) NOT NULL comment '密码',
   `deleted` boolean NOT NULL DEFAULT FALSE comment '是否删除',
   `create_time` datetime NOT NULL COMMENT '创建时间',
-  `create_by` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '创建用户',
+  `create_by` varchar(16) NOT NULL comment '创建用户',
   `update_time` datetime NOT NULL COMMENT '最后修改时间',
-  `update_by` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci unique comment '更新用户',
+  `update_by` varchar(16) NOT NULL  comment '更新用户',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户表';
 
 insert into user value (1, 1, 'yuri', '$2a$10$b2nCP59gVbx1Eqhw6c2p2OS6jlbX.8GrjziRuGNjTQ.1lKuU5y3f.', 0, NOW(), 1, NOW(), 1);
 
-SELECT
-    TABLE_COMMENT
-FROM
-    INFORMATION_SCHEMA.TABLES
-WHERE
-    TABLE_SCHEMA = 'zyl'
-  AND TABLE_NAME = 'user';
+DROP TABLE IF EXISTS `system_file`;
+CREATE TABLE `system_file` (
+  `id` bigint NOT NULL COMMENT '文件id',
+  `type` tinyint(2) NOT NULL DEFAULT 0 comment '文件类型: 默认其他类型',
+  `filename` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL comment '文件名',
+  `path` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL comment '路径',
+  `md5` varchar(32) NOT NULL comment '文件md5值',
+  `deleted` boolean NOT NULL DEFAULT FALSE comment '是否删除',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `create_by` varchar(16) NOT NULL comment '创建用户',
+  `update_time` datetime NOT NULL COMMENT '最后修改时间',
+  `update_by` varchar(16) NOT NULL comment '更新用户',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='文件列表';
 
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'zyl' AND TABLE_NAME = 'user';
-
-SELECT
-    COLUMN_NAME AS `field`,
-    COLUMN_TYPE AS `type`,
-    COLUMN_COMMENT AS `comment`,
-    TABLE_COMMENT AS `table_comment`
-FROM
-    INFORMATION_SCHEMA.COLUMNS as col
-    left join INFORMATION_SCHEMA.TABLES as tab on col.TABLE_NAME = tab.TABLE_NAME
-WHERE
-    col.TABLE_SCHEMA = 'zyl' AND col.TABLE_NAME = 'user' AND
-    tab.TABLE_SCHEMA = 'zyl' AND tab.TABLE_NAME = 'user';
-
-select * from user;
+select * from system_file;
