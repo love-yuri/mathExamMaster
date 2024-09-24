bc46117b2965a5a6aefc7cad20952a86e710829c
love-yuri <yuri2078170658@gmail.com>
2024-09-24 21:48:35 +0800

    feat(systemFileApi): 添加wmf转jpg功能

--- admin/root/src/api/systemFileApi.ts
+++ admin/root/src/api/systemFileApi.ts
@@ -2,12 +2,14 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-09-11 23:39:11
- * @LastEditTime: 2024-09-21 17:56:17
+ * @LastEditTime: 2024-09-23 23:05:34
  * @Description:
  */
 import { BaseApi } from '#/common/base/baseApi/baseApi';
+import { requestClient } from '#/common/base/baseApi/request';
 import { type BaseEntity } from '#/common/base/baseApi/types';
 import { useAppConfig } from '@vben/hooks';
+import type { UploadFileParam } from '@vben/request';
 
 const { apiURL } = useAppConfig(import.meta.env, import.meta.env.PROD);
 
@@ -26,6 +28,13 @@ class Api extends BaseApi<SystemFile> {
    * @param fileId 文件id
    */
   getFile = (fileId: string) => `${apiURL}${this.baseUrl}/get/${fileId}`;
+
+  /**
+   * wmf文件转jpg
+   */
+  wmfToJpg = (params: UploadFileParam): Promise<SystemFile> => {
+    return requestClient.upload(`${this.baseUrl  }/wmf/to/jpg`, params);
+  }
 
 }
 
