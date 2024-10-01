c077632cd0ae16582ff866cb98a061d6bc5bf66e
love-yuri <yuri2078170658@gmail.com>
2024-10-01 22:32:50 +0800

    feat(system): 修改系统路由，添加题库管理功能

--- admin/root/src/router/routes/modules/system.ts
+++ admin/root/src/router/routes/modules/system.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-09-10 23:35:05
- * @LastEditTime: 2024-09-10 23:39:53
+ * @LastEditTime: 2024-10-01 21:44:58
  * @Description: 系统路由
  */
 import type { RouteRecordRaw } from 'vue-router';
@@ -17,7 +17,7 @@ const routes: RouteRecordRaw[] = [
       title: '系统管理',
     },
     name: 'system',
-    path: '/',
+    path: '/system',
     children: [
       {
         name: 'generateCode',
@@ -26,7 +26,17 @@ const routes: RouteRecordRaw[] = [
         meta: {
           affixTab: false,
           icon: 'lucide:area-chart',
-          title: '代码生成',
+          title: '输入测试',
+        },
+      },
+      {
+        name: 'Question Bank Management',
+        path: '/question/bank',
+        component: () => import('#/views/system/questionBank/index.vue'),
+        meta: {
+          affixTab: false,
+          icon: 'lucide:area-chart',
+          title: '题库管理',
         },
       },
     ],
