361be1ff8211e353ac9a123cc90e1138818cf511
love-yuri <yuri2078170658@gmail.com>
2024-10-02 20:04:37 +0800

    feat: 添加通用组件导出

---
+++ admin/root/src/components/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-10-02 19:23:36
+ * @LastEditTime: 2024-10-02 19:24:47
+ * @Description: 通用组件导出
+ */
+
+export { default as Button } from 'primevue/button';
+export { default as InputText } from 'primevue/inputtext';
+export { default as Select } from 'primevue/select';
