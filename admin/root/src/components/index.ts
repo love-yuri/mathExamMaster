335bd74682665dbff117ebb8c67c05041eef93b1
love-yuri <yuri2078170658@gmail.com>
2024-10-03 22:56:30 +0800

    feat: 添加 FloatLabel 和 Textarea 组件导出

--- admin/root/src/components/index.ts
+++ admin/root/src/components/index.ts
@@ -1,10 +1,12 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-10-02 19:23:36
- * @LastEditTime: 2024-10-02 19:24:47
+ * @LastEditTime: 2024-10-03 19:33:50
  * @Description: 通用组件导出
  */
 
 export { default as Button } from 'primevue/button';
+export { default as FloatLabel } from 'primevue/floatlabel';
 export { default as InputText } from 'primevue/inputtext';
 export { default as Select } from 'primevue/select';
+export { default as Textarea } from 'primevue/textarea';
