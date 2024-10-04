9afb0455d390384b6372deae1ba3d08f8b00c2c8
love-yuri <yuri2078170658@gmail.com>
2024-10-04 21:58:06 +0800

    feat: 添加BasePage类以实现分页功能

---
+++ kotlin/src/main/kotlin/math/yl/love/common/mybatis/BasePage.kt
@@ -0,0 +1,19 @@
+package math.yl.love.common.mybatis
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+
+@Serializable
+class BasePage<T>(
+    @Transient
+    val page: Page<T> = Page()
+) {
+    constructor(): this(Page())
+    val current: Long = page.current
+    val size: Long = page.size
+    val records: List<T> = page.records
+    val total: Long = page.total
+}
+
