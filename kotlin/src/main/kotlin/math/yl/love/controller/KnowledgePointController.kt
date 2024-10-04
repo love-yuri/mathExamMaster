002f1db2f654c98b3cfce94bea2b11dbd22ac213
love-yuri <yuri2078170658@gmail.com>
2024-10-04 21:59:10 +0800

    feat: 添加分页功能至知识点控制器

--- kotlin/src/main/kotlin/math/yl/love/controller/KnowledgePointController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/KnowledgePointController.kt
@@ -1,14 +1,24 @@
 package math.yl.love.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import math.yl.love.common.base.R
 import math.yl.love.common.mybatis.BaseController
 import math.yl.love.database.domain.entity.KnowledgePoint
 import math.yl.love.database.mapper.KnowledgePointMapper
 import math.yl.love.database.service.KnowledgePointService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/knowledge/point")
 @Tag(name = "知识点")
-class KnowledgePointController: BaseController<KnowledgePoint, KnowledgePointMapper, KnowledgePointService>()
+class KnowledgePointController: BaseController<KnowledgePoint, KnowledgePointMapper, KnowledgePointService>() {
+
+    @PostMapping("page")
+    @Operation(summary = "分页")
+    fun page(@RequestBody param: PageParam) = R.success(baseService.page(param.current, param.size))
+
+}
--- kotlin/src/main/kotlin/math/yl/love/database/service/KnowledgePointService.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/service/KnowledgePointService.kt
@@ -1,6 +1,9 @@
 package math.yl.love.database.service
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
+import math.yl.love.common.mybatis.BasePage
 import math.yl.love.common.mybatis.BaseService
+import math.yl.love.common.utils.JsonUtils.toJson
 import math.yl.love.database.domain.entity.KnowledgePoint
 import math.yl.love.database.mapper.KnowledgePointMapper
 import org.springframework.stereotype.Service
@@ -11,4 +14,15 @@ import kotlin.reflect.KClass
 @Transactional(readOnly = true)
 class KnowledgePointService: BaseService<KnowledgePoint, KnowledgePointMapper>() {
     override val entityClass: KClass<KnowledgePoint> get() = KnowledgePoint::class
+
+    /**
+     * 分页数据
+     * @param current 当前页码
+     * @param size 每页大小
+     */
+     fun page(current: Long, size: Long): BasePage<KnowledgePoint> {
+        val p = Page<KnowledgePoint>(current, size)
+        val res = baseMapper.selectPage(p, queryWrapper)
+        return BasePage<KnowledgePoint>(res)
+    }
 }
