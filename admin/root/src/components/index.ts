2588512acb02cc78435db49939ce8b812782f444
love-yuri <yuri2078170658@gmail.com>
2024-10-06 23:06:15 +0800

    feat: 添加通用组件导出

--- admin/root/src/components/index.ts
+++ admin/root/src/components/index.ts
@@ -1,12 +1,32 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-10-02 19:23:36
- * @LastEditTime: 2024-10-03 19:33:50
+ * @LastEditTime: 2024-10-06 22:35:14
  * @Description: 通用组件导出
  */
 
+export { default as WangEditor } from './wangEditor/index.vue';
 export { default as Button } from 'primevue/button';
+export { default as Calendar } from 'primevue/calendar';
+export { default as Checkbox } from 'primevue/checkbox';
+export { default as Column } from 'primevue/column';
+export { default as ColumnGroup } from 'primevue/columngroup';
+export { default as ConfirmDialog } from 'primevue/confirmdialog';
+export { default as DataTable } from 'primevue/datatable';
+export { default as Dialog } from 'primevue/dialog';
+export { default as Divider } from 'primevue/divider';
+export { default as Dropdown } from 'primevue/dropdown';
+export { default as FileUpload } from 'primevue/fileupload';
 export { default as FloatLabel } from 'primevue/floatlabel';
+export { default as InputSwitch } from 'primevue/inputswitch';
 export { default as InputText } from 'primevue/inputtext';
+export { default as Menu } from 'primevue/menu';
+export { default as Message } from 'primevue/message';
+export { default as ProgressSpinner } from 'primevue/progressspinner';
 export { default as Select } from 'primevue/select';
+export { default as Tab } from 'primevue/tab';
+export { default as TabList } from 'primevue/tablist';
+export { default as TabPanel } from 'primevue/tabpanel';
+export { default as Tabs } from 'primevue/tabs';
 export { default as Textarea } from 'primevue/textarea';
+export { default as Toast } from 'primevue/toast';
