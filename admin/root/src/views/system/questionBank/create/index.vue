6be2b3f272fcaa87de8b28d1d9d6169df3d2e5a3
love-yuri <yuri2078170658@gmail.com>
2024-10-07 19:19:34 +0800

    feat(system/questionBank): 添加题目创建功能

--- admin/root/src/views/system/questionBank/create/index.vue
+++ admin/root/src/views/system/questionBank/create/index.vue
@@ -1,27 +1,43 @@
+<!--
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-10-06 19:57:12
+ * @LastEditTime: 2024-10-07 14:44:55
+ * @Description: 创建题目
+-->
 <template>
   <div class="p-2">
     <div class="mb-2 flex items-center">
       <Button
-        v-for="(item, index) in questionTypes"
+        v-for="(item, index) in questionComponents"
         :key="index"
-        :label="item"
+        :label="item.title"
         :severity="qaType === index ? 'success' : 'secondary'"
         class="mr-2"
         icon="pi pi-check"
         @click="qaType = index"
       />
     </div>
-    <div class="flex flex-col">
-      <div class="flex-shrink-0 text-2xl">请输入题目...</div>
-      <WangEditor />
-    </div>
+    <component :is="questionComponents[qaType]!.component" />
   </div>
 </template>
 <script setup lang="ts">
-import { Button, InputText, WangEditor } from '#/components';
-import { ref } from 'vue';
+import { QuestionTypeEnum } from '#/api/questionBankApi';
+import SingleChoise from './components/singleChoice.vue';
+import { Button } from '#/components';
+import { type Component, ref } from 'vue';
 
-const questionTypes = ['单选题', '多选题', '判断题', '填空题', '简答题'];
+type QuestionComponent = {
+  component: Component;
+  title: string;
+  type: string;
+};
 
 const qaType = ref(0);
+const questionComponents: QuestionComponent[] = [
+  {
+    component: SingleChoise,
+    title: '单选题',
+    type: QuestionTypeEnum.SINGLE_CHOICE,
+  },
+];
 </script>
