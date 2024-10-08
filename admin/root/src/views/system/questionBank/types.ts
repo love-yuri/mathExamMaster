9c2d70e12f56760173381e9c61ee71b5bee3a376
love-yuri <yuri2078170658@gmail.com>
2024-10-08 21:41:22 +0800

    feat(types.ts): 添加单选题和多选题JSON数据接口

--- admin/root/src/views/system/questionBank/types.ts
+++ admin/root/src/views/system/questionBank/types.ts
@@ -1,11 +1,37 @@
+/**
+ * 单选题json数据
+ */
 export interface SingleChoiceAnswer {
   /**
    * 正确答案
+   * 选项的顺序 从0开始
    */
-  answer: number;
+  answer?: number;
 
   /**
    * 选项列表
    */
-  keys: { value: string }[];
+  keys: {
+    // 选项的值
+    value: string;
+  }[];
+}
+
+/**
+ * 多选题json数据
+ */
+export interface MultipleChoiceAnswer {
+  /**
+   * 正确答案
+   * 选项的顺序 从0开始
+   */
+  answer: number[];
+
+  /**
+   * 选项列表
+   */
+  keys: {
+    // 选项的值
+    value: string;
+  }[];
 }
