55a00afbb4f41d7e2c28f96ed4a5b120711580c4
love-yuri <yuri2078170658@gmail.com>
2024-10-08 21:42:03 +0800

    feat(TestController): 添加答案测试功能

--- kotlin/src/main/kotlin/math/yl/love/controller/TestController.kt
+++ kotlin/src/main/kotlin/math/yl/love/controller/TestController.kt
@@ -5,8 +5,12 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport
 import io.swagger.v3.oas.annotations.tags.Tag
 import math.yl.love.common.base.R
+import math.yl.love.common.utils.JsonUtils.parseJson
 import math.yl.love.database.domain.entity.KnowledgePoint
+import math.yl.love.database.domain.result.questionBank.SingleChoiceAnswer
+import math.yl.love.database.domain.typeEnum.QuestionTypeEnum
 import math.yl.love.database.mapper.KnowledgePointMapper
+import math.yl.love.database.service.QuestionBankService
 import math.yl.love.database.service.UserService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,7 +24,8 @@ import org.springframework.web.bind.annotation.RestController
 @Tag(name = "测试")
 class TestController(
     private val userService: UserService,
-    private val knowledgePointMapper: KnowledgePointMapper
+    private val knowledgePointMapper: KnowledgePointMapper,
+    private val questionBankService: QuestionBankService
 ) {
 
     private val log = LoggerFactory.getLogger(TestController::class.java)
@@ -44,5 +49,17 @@ class TestController(
             records: ${res.records}
             gget ${res.records.size}
         """.trimIndent())
+    }
+
+    @PostMapping("answer/test")
+    fun answerTest() {
+        val list = questionBankService.list().filter { it.type == QuestionTypeEnum.SINGLE_CHOICE }
+        list.forEach {
+            val answer = it.answer.parseJson<SingleChoiceAnswer>()
+            log.info("""
+                答案 -> ${answer.answer}
+                选项 -> ${answer.keys}
+            """.trimIndent())
+        }
     }
 }
