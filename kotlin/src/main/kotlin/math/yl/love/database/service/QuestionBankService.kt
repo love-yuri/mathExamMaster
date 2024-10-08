781162a98cee804da8f621ff456036510a6cb17c
love-yuri <yuri2078170658@gmail.com>
2024-10-08 21:42:15 +0800

    feat(service): 添加根据题库类型获取答案的功能

--- kotlin/src/main/kotlin/math/yl/love/database/service/QuestionBankService.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/service/QuestionBankService.kt
@@ -1,7 +1,12 @@
 package math.yl.love.database.service
 
 import math.yl.love.common.mybatis.BaseService
+import math.yl.love.common.utils.JsonUtils
+import math.yl.love.common.utils.JsonUtils.parseJson
+import math.yl.love.configuration.exception.BizException
 import math.yl.love.database.domain.entity.QuestionBank
+import math.yl.love.database.domain.result.questionBank.SingleChoiceAnswer
+import math.yl.love.database.domain.typeEnum.QuestionTypeEnum
 import math.yl.love.database.mapper.QuestionBankMapper
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,4 +16,12 @@ import kotlin.reflect.KClass
 @Transactional(readOnly = true)
 class QuestionBankService: BaseService<QuestionBank, QuestionBankMapper>() {
     override val entityClass: KClass<QuestionBank> get() = QuestionBank::class
+
+    fun  getAnswer(questionBank: QuestionBank): Any {
+        return when(questionBank.type) {
+            QuestionTypeEnum.SINGLE_CHOICE -> questionBank.answer.parseJson<SingleChoiceAnswer>()
+            QuestionTypeEnum.SHORT_ANSWER -> questionBank.answer.parseJson<String>()
+            else -> throw BizException("未知题型...")
+        }
+    }
 }
