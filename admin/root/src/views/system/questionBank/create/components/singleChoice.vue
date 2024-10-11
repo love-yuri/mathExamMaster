a712249070831e2d37921a098c5f6fdcc0ba7a1e
love-yuri <yuri2078170658@gmail.com>
2024-10-11 21:10:47 +0800

    feat(system/questionBank): 重置答案功能与知识点多选添加

--- admin/root/src/views/system/questionBank/create/components/singleChoice.vue
+++ admin/root/src/views/system/questionBank/create/components/singleChoice.vue
@@ -6,6 +6,41 @@
         placeholder="请输入题目..."
       />
     </div>
+    <MultiSelect
+      v-model="selectedKnowledgePoints"
+      :options="knowledgePoints"
+      class="my-2 w-full"
+      filter
+      option-label="name"
+      placeholder="请选择关联知识点..."
+    />
+    <div class="my-3 flex">
+      <Button
+        class=""
+        icon="pi pi-plus"
+        label="添加选项"
+        severity="info"
+        @click="
+          answer.keys.push({
+            value: '',
+          })
+        "
+      />
+      <Button
+        class="mx-2"
+        icon="pi pi-plus"
+        label="创建题目"
+        severity="success"
+        @click="create"
+      />
+      <Button
+        class="mr-2"
+        icon="pi pi-sync"
+        label="重置答案"
+        severity="danger"
+        @click="cleanQuestion"
+      />
+    </div>
     <div
       v-for="(item, index) in answer.keys"
       :key="index"
@@ -26,26 +61,7 @@
         @click="removeKey(index)"
       />
     </div>
-    <div class="my-3 flex">
-      <Button
-        class=""
-        icon="pi pi-plus"
-        label="添加选项"
-        severity="info"
-        @click="
-          answer.keys.push({
-            value: '',
-          })
-        "
-      />
-      <Button
-        class="mx-2"
-        icon="pi pi-plus"
-        label="创建题目"
-        severity="success"
-        @click="create"
-      />
-    </div>
+
     <div class="flex flex-col">
       <div class="mb-2 text-center text-xl font-semibold">
         请选择正确答案...
@@ -78,16 +94,36 @@ import {
   questionBankApi,
   QuestionTypeEnum,
 } from '#/api/questionBankApi';
-import { Button, InputText, RadioButton, WangEditor } from '#/components';
-import { ref } from 'vue';
+import {
+  Button,
+  InputText,
+  MultiSelect,
+  RadioButton,
+  WangEditor,
+} from '#/components';
+import { onMounted, ref } from 'vue';
 import { checkEmpty, checkSuccess } from '#/common/utils/valueCheck';
 import message from '#/common/utils/message';
+import {
+  type KnowledgePoint,
+  knowledgePointApi,
+} from '#/api/knowledgePointApi';
 
 const question = ref(new QuestionBank(QuestionTypeEnum.SINGLE_CHOICE));
 const answer = ref<SingleChoiceAnswer>({
   answer: undefined,
   keys: [],
 });
+
+/**
+ * 处理知识点选择
+ */
+const knowledgePoints = ref<KnowledgePoint[]>([]);
+const selectedKnowledgePoints = ref<KnowledgePoint[]>([]);
+const loadKnowledgePoints = async () => {
+  const res = await knowledgePointApi.list();
+  knowledgePoints.value = res;
+};
 
 /**
  * 创建题目
@@ -105,7 +141,14 @@ function create() {
     return;
   }
   question.value.answer = JSON.stringify(answer.value);
-  checkSuccess(questionBankApi.create(question.value), true, '题目');
+  checkSuccess(
+    questionBankApi.save({
+      knowledgePointIds: selectedKnowledgePoints.value.map((it) => it.id!),
+      questionBank: question.value,
+    }),
+    true,
+    '题目',
+  );
 }
 
 /**
@@ -124,4 +167,21 @@ function removeKey(index: number) {
     answer.value.answer = answerIndex - 1;
   }
 }
+
+/**
+ * 清空题目
+ */
+function cleanQuestion() {
+  question.value.reset();
+  selectedKnowledgePoints.value.length = 0;
+  answer.value.answer = undefined;
+  answer.value.keys.length = 0;
+}
+
+/**
+ * 挂载时加载
+ */
+onMounted(() => {
+  loadKnowledgePoints();
+});
 </script>
