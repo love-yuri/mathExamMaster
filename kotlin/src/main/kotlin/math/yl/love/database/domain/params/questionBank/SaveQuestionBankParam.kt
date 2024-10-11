7e9762faf85aca067f1a277045bed137428553d0
love-yuri <yuri2078170658@gmail.com>
2024-10-11 21:12:24 +0800

    feat: 添加题目和知识点属性描述

--- kotlin/src/main/kotlin/math/yl/love/database/domain/params/questionBank/SaveQuestionBankParam.kt
+++ kotlin/src/main/kotlin/math/yl/love/database/domain/params/questionBank/SaveQuestionBankParam.kt
@@ -1,11 +1,12 @@
 package math.yl.love.database.domain.params.questionBank
 
+import io.swagger.v3.oas.annotations.media.Schema
 import kotlinx.serialization.Serializable
 import math.yl.love.database.domain.entity.KnowledgePoint
 import math.yl.love.database.domain.entity.QuestionBank
 
 @Serializable
 data class SaveQuestionBankParam(
-    val questionBank: QuestionBank,
-    val knowledgePointIds: List<String>
+    @Schema(description = "题目") val questionBank: QuestionBank,
+    @Schema(description = "知识点") val knowledgePointIds: List<String>
 )
