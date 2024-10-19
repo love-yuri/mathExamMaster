6dba273a83ab0438f61497000e2efb93d2e64067
love-yuri <yuri2078170658@gmail.com>
2024-10-19 21:05:59 +0800

    feat(components): 添加确认对话框组件，支持自定义内容和操作，优化用户交互体验

---
+++ admin/root/src/components/confirmDialog/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <ConfirmDialog :group="group">
+    <template #container="{ message, acceptCallback, rejectCallback }">
+      <div
+        class="bg-surface-0 dark:bg-surface-900 flex flex-col items-center rounded p-8"
+      >
+        <p class="mb-0">{{ message.message }}</p>
+        <div class="mt-6 flex items-center gap-2">
+          <Button
+            class="w-32"
+            icon="pi pi-times"
+            label="取消"
+            outlined
+            severity="secondary"
+            @click="rejectCallback"
+          />
+          <Button
+            class="w-32"
+            icon="pi pi-trash"
+            label="删除"
+            severity="danger"
+            @click="acceptCallback"
+          />
+        </div>
+      </div>
+    </template>
+  </ConfirmDialog>
+</template>
+<script setup lang="ts">
+import { Button, ConfirmDialog } from '#/components';
+
+defineProps<{
+  group: string;
+}>();
+</script>
