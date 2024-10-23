550acbdb65b0962ee7bdd7ea8ad6ee4efb32c964
love-yuri <yuri2078170658@gmail.com>
2024-10-23 21:51:49 +0800

    feat(admin.router): 更新路由配置，调整固定标签功能，优化用户体验

--- admin/root/src/router/routes/modules/dashboard.ts
+++ admin/root/src/router/routes/modules/dashboard.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-09-08 15:42:02
- * @LastEditTime: 2024-09-09 13:48:03
+ * @LastEditTime: 2024-10-23 21:09:33
  * @Description: 路由
  */
 import type { RouteRecordRaw } from 'vue-router';
@@ -25,7 +25,7 @@ const routes: RouteRecordRaw[] = [
         path: '/analytics',
         component: () => import('#/views/dashboard/analytics/index.vue'),
         meta: {
-          affixTab: true,
+          affixTab: false,
           icon: 'lucide:area-chart',
           title: '后台管理',
         },
