cc9c31265eacc67036c5f38d2f11047469f097b5
love-yuri <yuri2078170658@gmail.com>
2024-10-25 18:25:06 +0800

    feat: 更新生产环境配置和添加scp脚本

---
+++ script/scp.sh
@@ -0,0 +1,8 @@
+###
+ # @Author: love-yuri yuri2078170658@gmail.com
+ # @Date: 2024-10-25 18:17:45
+ # @LastEditTime: 2024-10-25 18:19:52
+ # @Description: scp 命令
+### 
+scp ./kotlin/target/math-0.0.1-SNAPSHOT.jar yuri@47.98.104.121:/home/yuri/math/
+scp -r ./admin/root/dist yuri@47.98.104.121:/home/yuri/math/admin
