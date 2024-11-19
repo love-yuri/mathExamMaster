7dfa5c233fbd333127aa847c959af1eaede0d918
love-yuri <yuri2078170658@gmail.com>
2024-11-19 20:48:23 +0800

    refactor(admin/router): 重命名并调整试卷管理路由至考试页面

--- admin/root/src/router/routes/modules/examPage.ts
+++ admin/root/src/router/routes/modules/examPage.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-09-10 23:35:05
- * @LastEditTime: 2024-10-28 17:48:16
+ * @LastEditTime: 2024-11-19 20:17:50
  * @Description: 系统路由
  */
 import type { RouteRecordRaw } from 'vue-router';
@@ -16,13 +16,23 @@ const routes: RouteRecordRaw[] = [
       order: 2,
       title: '试卷管理',
     },
-    name: 'testPaperManager',
-    path: '/testPaperManager',
+    name: 'examPageManager',
+    path: '/examPageManager',
     children: [
       {
-        name: 'testPaperCreate',
-        path: '/test/paper/create',
-        component: () => import('#/views/testPaper/create/index.vue'),
+        name: 'examPageManager',
+        path: '/exam/page/manager',
+        component: () => import('#/views/examPage/index.vue'),
+        meta: {
+          affixTab: false,
+          icon: 'lucide:copyright',
+          title: '试卷管理',
+        },
+      },
+      {
+        name: 'examPageCreate',
+        path: '/exam/page/create',
+        component: () => import('#/views/examPage/create/index.vue'),
         meta: {
           affixTab: false,
           icon: 'lucide:copyright',
