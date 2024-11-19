c60e22d398c32a6482c6c2ba519397184a77c77b
love-yuri <yuri2078170658@gmail.com>
2024-11-19 20:47:38 +0800

    feat(examPageApi): 添加分页获取试卷接口

--- admin/root/src/api/examPageApi.ts
+++ admin/root/src/api/examPageApi.ts
@@ -1,15 +1,30 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-09-11 23:39:11
- * @LastEditTime: 2024-11-15 18:35:16
+ * @LastEditTime: 2024-11-19 19:33:29
  * @Description:
  */
-import { BaseApi } from '#/common/base/baseApi/baseApi';
+import {
+  BaseApi,
+  type PageParam,
+  type PageResult,
+} from '#/common/base/baseApi/baseApi';
 import { type BaseEntity, RequestType } from '#/common/base/baseApi/types';
-import { ExamPageCreateVO } from '#/views/testPaper/types';
+import { ExamPageCreateVO, ExamPageResult } from '#/views/examPage/types';
 
 class Api extends BaseApi<BaseEntity> {
   override baseUrl: string = '/exam/page';
+
+  /**
+   * 分页获取试卷
+   */
+  pageSimple = (param: PageParam) => {
+    return this.add<PageResult<ExamPageResult>>(
+      RequestType.POST,
+      '/page/simple',
+      param,
+    );
+  };
 
   /**
    * 发布试卷
