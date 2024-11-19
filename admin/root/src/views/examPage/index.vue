9645e49f6d428ffbde885d03072a4b21ea43a38d
love-yuri <yuri2078170658@gmail.com>
2024-11-19 20:48:36 +0800

    feat(examPage): 新增题库管理页面

---
+++ admin/root/src/views/examPage/index.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="p-2">
+    <Card>
+      <template #content>
+        <DataTable :value="pages" scrollable table-style="min-width: 50rem">
+          <template #header>
+            <div class="flex flex-wrap items-center justify-between gap-2">
+              <span class="text-xl font-bold">题库</span>
+              <Button
+                icon="pi pi-plus"
+                raised
+                rounded
+                @click="router.push('/exam/page/create')"
+              />
+            </div>
+          </template>
+          <Column header="试卷标题" style="min-width: 110px">
+            <template #body="slotProps: { data: ExamPageResult }">
+              <EllipsisText :max-width="500">
+                {{ slotProps.data.title }}
+              </EllipsisText>
+            </template>
+          </Column>
+          <Column header="试卷类型" style="min-width: 110px">
+            <template #body="slotProps: { data: ExamPageResult }">
+              <Tag
+                :severity="ExamPageTypeColorMap[slotProps.data.type]"
+                :value="ExamPageMap[slotProps.data.type]"
+              />
+            </template>
+          </Column>
+          <Column header="所属科目" style="min-width: 110px">
+            <template #body="slotProps: { data: ExamPageResult }">
+              <Tag
+                :severity="SubjectTypeColorMap[slotProps.data.subject]"
+                :value="SubjectTypeMap[slotProps.data.subject]"
+              />
+            </template>
+          </Column>
+          <Column field="releaseTime" header="发布时间" />
+          <Column field="action" header="" style="min-width: 110px">
+            <template #body="slotProps: { data: ExamPageResult }">
+              <SplitButton
+                :model="[
+                  {
+                    label: '编辑',
+                    icon: 'pi pi-pencil',
+                    command: () => {},
+                  },
+                  {
+                    label: '删除',
+                    icon: 'pi pi-trash',
+                    command: () => remove(slotProps.data.id!),
+                  },
+                ]"
+                icon="pi pi-eye"
+                label="预览"
+                raised
+                severity="info"
+              />
+            </template>
+          </Column>
+        </DataTable>
+        <Paginator
+          :rows="pageParam.size"
+          :rows-per-page-options="[10, 20, 30]"
+          :total-records="pageParam.total"
+          class="mt-2"
+          @page="onPage"
+        />
+      </template>
+    </Card>
+    <DefaultConfirmDialog group="headless" />
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import {
+  Button,
+  Card,
+  Column,
+  DataTable,
+  DefaultConfirmDialog,
+  Paginator,
+  SplitButton,
+  Tag,
+} from '#/components';
+import {
+  ExamPageMap,
+  ExamPageResult,
+  ExamPageType,
+  SubjectType,
+  SubjectTypeMap,
+} from '#/views/examPage/types';
+import { examPageApi } from '#/api/examPageApi';
+import type { PageParam } from '#/common/base/baseApi/baseApi';
+import type { PageState } from 'primevue/paginator';
+import { useConfirm } from 'primevue/useconfirm';
+import { router } from '#/router';
+import { EllipsisText } from '@vben/common-ui';
+import message from '#/common/utils/message';
+
+const confirm = useConfirm();
+const pageParam = ref<PageParam>({
+  current: 1,
+  size: 10,
+  total: 0,
+});
+
+const pages = ref<ExamPageResult[]>([]);
+
+const ExamPageTypeColorMap = {
+  [ExamPageType.DEFULT]: 'primary',
+};
+
+const SubjectTypeColorMap = {
+  [SubjectType.HighMath]: 'success',
+};
+
+/**
+ * 页码改变
+ */
+function onPage(p: PageState) {
+  pageParam.value.current = p.page + 1;
+  pageParam.value.size = p.rows;
+  loadData();
+}
+
+async function loadData() {
+  examPageApi.pageSimple(pageParam.value).then((res) => {
+    pages.value = res.records;
+    pageParam.value.total = res.total;
+  });
+}
+
+onMounted(loadData);
+
+/**
+ * 删除
+ * @param id 题目id
+ */
+function remove(id: string) {
+  confirm.require({
+    accept: () => {
+      examPageApi.delete(id).then(() => {
+        message.success('删除成功');
+        loadData();
+      });
+    },
+    group: 'headless',
+    message: '是否要删除该试卷?',
+  });
+}
+</script>
