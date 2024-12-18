4bc63671a3f6cc536125afc813909b0d75cef092
love-yuri <yuri2078170658@gmail.com>
2024-12-18 20:19:18 +0800

    feat(examPage): 添加试卷结果与Service实现

---
+++ kotlin/src/main/kotlin/math/yl/love/database/domain/result/examPage/ExamPageResult.kt
@@ -0,0 +1,46 @@
+package math.yl.love.database.domain.result.examPage
+
+import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.LongAsStringSerializer
+import math.yl.love.database.domain.params.examPage.ExamPageQuestion
+import math.yl.love.database.domain.result.questionBank.FullQuestionBank
+import java.time.LocalDateTime
+
+@Serializable
+data class ExamPageResult (
+
+    @Schema(description = "试卷 id")
+    @Serializable(with = LongAsStringSerializer::class)
+    val id: Long,
+
+    @Contextual
+    @Schema(description = "发布时间")
+    val releaseTime: LocalDateTime,
+
+    @Contextual
+    @Schema(description = "截止时间")
+    val deadline: LocalDateTime? = null,
+
+    @Schema(description = "难度")
+    val difficulty: Int,
+
+    @Schema(description = "考试限制时间")
+    val limitedTime: Int,
+
+    @Schema(description = "关联题目")
+    val questions: List<ExamPageQuestion>,
+
+    @Schema(description = "科目")
+    val subject: Int,
+
+    @Schema(description = "标题")
+    val title: String,
+
+    @Schema(description = "总分")
+    val totalScore: Int,
+
+    @Schema(description = "类型")
+    val type: Int,
+)
---
+++ kotlin/src/main/kotlin/math/yl/love/database/service/ExamPageService.kt
@@ -0,0 +1,168 @@
+package math.yl.love.database.service
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
+import math.yl.love.common.mybatis.BasePage
+import math.yl.love.common.mybatis.BaseService
+import math.yl.love.configuration.exception.BizException
+import math.yl.love.database.domain.entity.ExamPage
+import math.yl.love.database.domain.entity.ExamPageQuestionRelation
+import math.yl.love.database.domain.entity.ExamPageUserRelation
+import math.yl.love.database.domain.params.examPage.ExamPageQuestion
+import math.yl.love.database.domain.params.examPage.ReleasePageParam
+import math.yl.love.database.domain.result.examPage.ExamPageResult
+import math.yl.love.database.mapper.ExamPageMapper
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.io.Serializable
+import kotlin.reflect.KClass
+
+@Service
+@Transactional(readOnly = true)
+class ExamPageService(
+    private val examPageQuestionRelationService: ExamPageQuestionRelationService,
+    private val examPageUserRelationService: ExamPageUserRelationService,
+    private val questionBankService: QuestionBankService
+): BaseService<ExamPage, ExamPageMapper>() {
+
+    override val entityClass: KClass<ExamPage> get() = ExamPage::class
+
+    /**
+     * 发布试卷
+     * @param param 试卷参数
+     */
+    @Transactional(rollbackFor = [Exception::class])
+    fun release(param: ReleasePageParam): Boolean {
+        // 保存试卷
+        val page = ExamPage (
+            title = param.title,
+            subject = param.subject,
+            type = param.type,
+            totalScore = param.totalScore,
+            limitedTime = param.limitedTime,
+            difficulty = param.difficulty
+        )
+        save(page)
+
+        // 保存试卷-题目
+        val questions = param.questions.map {
+            ExamPageQuestionRelation(
+                examPageId = page.id!!,
+                questionBankId = it.questionBankId,
+                score = it.score
+            )
+        }
+        examPageQuestionRelationService.saveBatch(questions)
+
+        return true
+    }
+
+    /**
+     * 更新试卷
+     * @param param 试卷参数
+     */
+    @Transactional(rollbackFor = [Exception::class])
+    fun updatePage(param: ReleasePageParam): Boolean {
+        param.id ?: throw RuntimeException("id不能为空!!")
+        // 更新试卷
+        val page = ExamPage (
+            id = param.id,
+            title = param.title,
+            subject = param.subject,
+            type = param.type,
+            totalScore = param.totalScore,
+            limitedTime = param.limitedTime,
+            difficulty = param.difficulty
+        )
+        updateById(page)
+
+        // 更新试卷-题目
+        val oldQuestionMap = examPageQuestionRelationService.findByPageId(param.id).associateBy { it.questionBankId }.toMutableMap()
+
+        val updateQuestion = mutableListOf<ExamPageQuestionRelation>()
+        val newQuestion = mutableListOf<ExamPageQuestionRelation>()
+        param.questions.forEach {
+            // 如果存在就更新数据
+            if (oldQuestionMap.containsKey(it.questionBankId)) {
+                updateQuestion.add(ExamPageQuestionRelation(
+                    id = oldQuestionMap[it.questionBankId]!!.id!!,
+                    examPageId = page.id!!,
+                    questionBankId = it.questionBankId,
+                    score = it.score
+                ))
+                oldQuestionMap.remove(it.questionBankId)
+            } else {
+                newQuestion.add(ExamPageQuestionRelation(
+                    examPageId = page.id!!,
+                    questionBankId = it.questionBankId,
+                    score = it.score
+                ))
+            }
+        }
+
+        examPageQuestionRelationService.updateBatchById(updateQuestion)
+        examPageQuestionRelationService.saveBatch(newQuestion)
+        examPageQuestionRelationService.removeByIds(oldQuestionMap.values)
+        return true
+    }
+
+    /**
+     * 分页获取试卷详情
+     * @param current 当前页
+     * @param size 每页大小
+     */
+    fun pageSimple(current: Long, size: Long): BasePage<ExamPageResult> {
+        val pages = page(current, size)
+        val pageList = mutableListOf<ExamPageResult>()
+        pages.records.forEach {
+            pageList.add(setByExamPage(it))
+        }
+        return BasePage(current, size, pageList, pages.total)
+    }
+
+    /**
+     * 根据试卷信息获取详细数据
+     * @param page 试卷信息
+     */
+    private fun setByExamPage(page: ExamPage): ExamPageResult {
+        val questions = examPageQuestionRelationService.findByPageId(page.id!!)
+        return ExamPageResult(
+            id = page.id!!,
+            title = page.title,
+            difficulty = page.difficulty,
+            limitedTime = page.limitedTime,
+            subject = page.subject,
+            type = page.type,
+            releaseTime = page.createTime!!,
+            totalScore = page.totalScore,
+            questions = questions.map { k ->
+                ExamPageQuestion(
+                    questionBankId = k.questionBankId,
+                    score = k.score,
+                )
+            }
+        )
+    }
+
+    /**
+     * 删除试卷，需要同步删除另外两个试卷的数据
+     * TODO: 删除试卷还需要删除试卷发布
+     */
+    @Transactional(rollbackFor = [Exception::class])
+    override fun removeById(id: Serializable?): Boolean {
+        return super.removeById(id)
+                && examPageQuestionRelationService.deleteByPageId(id.toString().toLong())
+    }
+
+    /**
+     * 根据id获取详细数据
+     * @param id 试卷id
+     */
+    fun detail(id: Long): ExamPageResult {
+        val page = getById(id) ?: throw BizException("试卷不存在!!")
+        val result = setByExamPage(page)
+        result.questions.forEach {
+            it.fullQuestionBank = questionBankService.detail(it.questionBankId)
+        }
+        return result
+    }
+}
