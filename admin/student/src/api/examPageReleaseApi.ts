a442d5d9ebb3d45258a2e3ff9d5de6688bd46a4a
love-yuri <yuri2078170658@gmail.com>
2024-12-19 19:41:12 +0800

    feat(student/api): 添加试卷发布接口及参数类

--- admin/student/src/api/examPageReleaseApi.ts
+++ admin/student/src/api/examPageReleaseApi.ts
@@ -1,11 +1,12 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-09-11 23:39:11
- * @LastEditTime: 2024-12-18 18:52:24
+ * @LastEditTime: 2024-12-19 18:38:50
  * @Description:
  */
 import { BaseApi } from '#/common/base/baseApi/baseApi';
-import { type BaseEntity, RequestType } from '#/common/base/baseApi/types';
+import { BaseEntity, RequestType } from '#/common/base/baseApi/types';
+import type { ExamPageResult } from './examPageApi';
 
 export interface ExamListParam {
   mode: number;
@@ -17,8 +18,62 @@ export interface ExamListResult {
   name: string;
 }
 
+export class ExamPageReleaseParam extends BaseEntity {
+  endTime!: string;
+  examPageId!: string;
+  startTime!: string;
+  userIds!: string[];
+
+  /**
+   * 手动调用reset
+   */
+  constructor() {
+    super();
+    this.reset();
+  }
+
+  override reset(): void {
+    this.startTime = '';
+    this.endTime = '';
+    this.userIds = [];
+    this.examPageId = '';
+  }
+}
+
+/**
+ * 试卷发布结果
+ */
+export class ExamPageReleaseResult extends ExamPageReleaseParam {
+  examPage?: ExamPageResult;
+  users: {
+    id: string;
+    username: string;
+  }[] = [];
+
+  /**
+   * 手动调用reset
+   */
+  constructor() {
+    super();
+    this.reset();
+  }
+
+  override reset(): void {
+    super.reset();
+    this.users = [];
+    this.examPage = undefined;
+  }
+}
+
 class Api extends BaseApi<BaseEntity> {
   override baseUrl: string = '/exam/page/release';
+
+  /**
+   * 根据id获取详情
+   */
+  detail = (id: string) => {
+    return this.add<ExamPageReleaseResult>(RequestType.POST, `/detail/${id}`);
+  };
 
   /**
    * 获取练习列表
