58707458958d7e787732e49aaed642eb864cd511
love-yuri <yuri2078170658@gmail.com>
2024-12-23 19:43:44 +0800

    refactor(exam): 移除练习管理路由及doExam组件

--- admin/student/src/router/routes/modules/exam.ts
+++ admin/student/src/router/routes/modules/exam.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-09-10 23:35:05
- * @LastEditTime: 2024-12-19 18:27:40
+ * @LastEditTime: 2024-12-23 18:26:00
  * @Description: 系统路由
  */
 import type { RouteRecordRaw } from 'vue-router';
@@ -27,18 +27,6 @@ const routes: RouteRecordRaw[] = [
           affixTab: false,
           icon: 'lucide:copyright',
           title: '练习管理',
-        },
-      },
-      {
-        name: 'doExam',
-        path: String.raw`/do/exam/:id(\d+)`,
-        component: () => import('#/views/examManager/doExam.vue'),
-        meta: {
-          hideInBreadcrumb: true,
-          hideInMenu: true,
-          hideInTabs: true,
-          icon: 'lucide:copyright',
-          title: '',
         },
       },
     ],
