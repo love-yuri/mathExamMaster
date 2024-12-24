140a1cbd46a79bfd0b34438cfa6352fc368aac6f
love-yuri <yuri2078170658@gmail.com>
2024-12-24 20:04:03 +0800

    refactor(doExam.vue): 优化考试信息展示逻辑

--- admin/student/src/views/examManager/doExam.vue
+++ admin/student/src/views/examManager/doExam.vue
@@ -8,7 +8,7 @@
         <div class="min-w-[800px] space-y-6">
           <!-- 标题 -->
           <div class="text-center text-[32px] font-bold text-gray-800">
-            {{ currentPage.title }}
+            {{ examInfo?.title }}
           </div>
 
           <div class="grid grid-cols-2 gap-4">
@@ -19,7 +19,7 @@
               </div>
               <div class="info">
                 <span class="title">考试难度: </span>
-                <Rating :model-value="currentPage.difficulty" :stars="9" />
+                <Rating :model-value="examInfo?.difficulty" :stars="9" />
               </div>
               <div v-show="false" class="info">
                 <span class="title">试卷类型: </span>
@@ -27,58 +27,58 @@
               </div>
               <div class="info">
                 <span class="title">结束时间: </span>
-                <span class="content">{{ currentRelease.endTime }}</span>
+                <span class="content">{{ examInfo?.endTime }}</span>
               </div>
             </div>
 
             <div>
               <div class="info">
                 <span class="title">试卷总分: </span>
-                <span class="content">{{ currentPage.totalScore }} 分</span>
+                <span class="content">{{ examInfo?.totalScore }} 分</span>
               </div>
               <div class="info">
                 <span class="title">考试时长: </span>
                 <span class="content">
-                  {{ currentPage.limitedTime / 60 }} 分钟
+                  {{ (examInfo?.limitedTime ?? 1) / 60 }} 分钟
                 </span>
               </div>
             </div>
           </div>
-          <div class="flex justify-center">
+          <div class="flex flex-col items-center justify-center">
             <FameraButton @click="startExam">
               {{
-                currentRelation.status ===
-                ExamPageUserRelationStatusType.NOT_START
-                  ? '开始考试'
-                  : '继续考试'
+                // eslint-disable-next-line vue/eqeqeq
+                examNotStart ? '开始考试' : '继续考试'
               }}
             </FameraButton>
+            <div v-if="!examNotStart" class="mt-4 text-[22px]">
+              剩余时间:
+              <span class="font-semibold text-red-500">
+                {{ leftTimeText }}
+              </span>
+            </div>
           </div>
         </div>
       </template>
     </Card>
   </div>
-  <Examing v-else :exam-info="examInfo!!" />
+  <Examing v-else :exam-page-info="examPageInfo!!" />
 </template>
 <script setup lang="ts">
 import {
-  examPageApi,
   ExamPageMap,
-  ExamPageResult,
+  ExamPageType,
+  SubjectType,
   SubjectTypeMap,
 } from '#/api/examPageApi';
 import {
+  type ExamInfoResult,
   examPageReleaseApi,
-  ExamPageReleaseResult,
   type StartExamResult,
 } from '#/api/examPageReleaseApi';
-import { computed, ref, watchEffect } from 'vue';
+import { computed, onUnmounted, ref, watchEffect } from 'vue';
 import { Card, FameraButton, Rating } from '#/components';
-import {
-  ExamPageUserRelation,
-  examPageUserRelationApi,
-  ExamPageUserRelationStatusType,
-} from '#/api/examPageUserRelationApi';
+import { ExamPageUserRelationStatusType } from '#/api/examPageUserRelationApi';
 import Examing from './components/examing.vue';
 
 const { id } = defineProps<{
@@ -86,31 +86,90 @@ const { id } = defineProps<{
 }>();
 
 const isExam = ref(false);
-const examInfo = ref<StartExamResult>();
-const currentRelation = ref<ExamPageUserRelation>(new ExamPageUserRelation());
-const currentRelease = ref<ExamPageReleaseResult>(new ExamPageReleaseResult());
-const currentPage = ref<ExamPageResult>(new ExamPageResult());
-const examPageType = computed(() => ExamPageMap[currentPage.value.type]);
-const examPageSubject = computed(
-  () => SubjectTypeMap[currentPage.value.subject],
+const leftTime = ref(0);
+const examInfo = ref<ExamInfoResult>();
+const examPageInfo = ref<StartExamResult>();
+const examPageType = computed(
+  () => ExamPageMap[examInfo.value?.type ?? ExamPageType.DEFULT],
 );
+const examPageSubject = computed(
+  () => SubjectTypeMap[examInfo.value?.subject ?? SubjectType.HighMath],
+);
+
+const examNotStart = computed(() => {
+  // eslint-disable-next-line eqeqeq
+  return examInfo.value?.status == ExamPageUserRelationStatusType.NOT_START;
+});
+
+const leftTimeText = computed(() => {
+  const seconds = leftTime.value;
+  const hours = Math.floor(seconds / 3600); // 计算小时
+  const minutes = Math.floor((seconds % 3600) / 60); // 计算分钟
+  const secs = seconds % 60; // 剩余秒数
+
+  // 格式化为两位数
+  const pad = (num: number) => num.toString().padStart(2, '0');
+
+  return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+});
+
+let interval: null | ReturnType<typeof setInterval> = null;
 
 watchEffect(async () => {
   if (!id) {
     return;
   }
-  const release = await examPageReleaseApi.detail(id);
-  currentRelation.value.copy(
-    await examPageUserRelationApi.relation(release.id!!),
-  );
-  const page = await examPageApi.detail(release.examPage!!.id!!);
-  currentPage.value.copy(page);
-  currentRelease.value.copy(release);
+
+  // 获取考试信息
+  examInfo.value = await examPageReleaseApi.examInfo(id);
+  // 清理之前的定时器
+  if (interval) {
+    clearInterval(interval);
+  }
+  if (!examNotStart.value) {
+    // 计算剩余时间
+    leftTime.value =
+      examInfo.value.limitedTime -
+      calculateTimeDiff(examInfo.value!!.examStartTime!!);
+
+    // 定义定时器
+    interval = setInterval(() => {
+      leftTime.value--;
+      if (leftTime.value <= 0) {
+        clearInterval(interval!);
+        interval = null;
+      }
+    }, 1000);
+  }
 });
 
+// 监控 isExam 并清理定时器
+watchEffect(() => {
+  if (isExam.value && interval) {
+    clearInterval(interval);
+    interval = null;
+  }
+});
+
+// 在组件卸载时清理定时器
+onUnmounted(() => {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+  }
+});
+
+function calculateTimeDiff(endTime: string): number {
+  const start = Date.now(); // 转为时间戳（毫秒）
+  const end = new Date(endTime).getTime(); // 转为时间戳（毫秒）
+
+  const differenceInMilliseconds = start - end; // 时间差（毫秒）
+  return Math.floor(differenceInMilliseconds / 1000); // 转换为秒
+}
+
 function startExam() {
-  examPageReleaseApi.startExam(currentRelease.value.id!!).then((res) => {
-    examInfo.value = res;
+  examPageReleaseApi.startExam(examInfo.value!!.releaseId).then((res) => {
+    examPageInfo.value = res;
     isExam.value = true;
   });
 }
