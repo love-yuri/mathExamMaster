124a245093254fa850c5f5266a77b6793ecf1956
love-yuri <yuri2078170658@gmail.com>
2024-12-24 20:04:07 +0800

    fix(examManager): 修改examing组件中examInfo为examPageInfo

--- admin/student/src/views/examManager/components/examing.vue
+++ admin/student/src/views/examManager/components/examing.vue
@@ -1,12 +1,18 @@
+<!--
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-12-23 18:55:25
+ * @LastEditTime: 2024-12-24 19:10:06
+ * @Description: 
+-->
 <template>
   <div>
-    {{ examInfo }}
+    {{ examPageInfo }}
   </div>
 </template>
 <script lang="ts" setup>
 import type { StartExamResult } from '#/api/examPageReleaseApi';
 
 defineProps<{
-  examInfo: StartExamResult;
+  examPageInfo: StartExamResult;
 }>();
 </script>
