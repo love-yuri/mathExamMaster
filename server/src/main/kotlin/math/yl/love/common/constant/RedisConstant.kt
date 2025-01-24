b74e4e1214c0cfce07db39ebbae9f18c901a915b
love-yuri <yuri2078170658@gmail.com>
2025-01-24 16:45:22 +0800

    feat: 优化Redis缓存管理，添加getOrReSet方法

--- server/src/main/kotlin/math/yl/love/common/constant/RedisConstant.kt
+++ server/src/main/kotlin/math/yl/love/common/constant/RedisConstant.kt
@@ -7,10 +7,15 @@ object RedisConstant {
     /**
      * 程序通用前缀
      */
-    const val PREFIX = "love:yl:"
+    const val PREFIX = "love:yl"
 
     /**
      * 登录信息
      */
-    const val USER_INFO = "${PREFIX}user:info"
+    const val USER_INFO = "${PREFIX}:user:info"
+
+    /**
+     * 根据id获取系统文件信息
+     */
+    const val SYSTEM_FILE_GET = "${PREFIX}:system:file:get"
 }
--- server/src/main/kotlin/math/yl/love/database/service/RedisService.kt
+++ server/src/main/kotlin/math/yl/love/database/service/RedisService.kt
@@ -14,15 +14,23 @@ class RedisService(
     val stringRedisTemplate: StringRedisTemplate,
     val systemConfig: SystemConfig
 ) {
+
     /**
      * 设置缓存
+     * @param key key
+     * @param value value
+     * @param timeout 超时时间
      */
     final inline fun <reified T> set(
         key: String, // key
         value: T, // value
-        timeout: Long = systemConfig.redisDefaultTimeout
+        timeout: Long? = systemConfig.redisDefaultTimeout
     ) {
-        stringRedisTemplate.opsForValue().set(key, value.toJson(), timeout, TimeUnit.SECONDS)
+        if (timeout == null) {
+            stringRedisTemplate.opsForValue().set(key, value.toJson())
+        } else {
+            stringRedisTemplate.opsForValue().set(key, value.toJson(), timeout, TimeUnit.SECONDS)
+        }
     }
 
     /**
@@ -30,5 +38,20 @@ class RedisService(
      */
     final inline fun <reified T> get(key: String): T? {
         return stringRedisTemplate.opsForValue().get(key)?.parseJson()
+    }
+
+    /**
+     * 获取缓存里的数据，如果不存在则调用函数重新获取
+     * @param f 获取数据的函数 返回许哟啊获取的数据
+     * @param timeout 过期时间，默认为null
+     */
+    final inline fun <reified T> getOrReSet(key: String, f: () -> T, timeout: Long? = systemConfig.redisDefaultTimeout): T {
+        val data = get<T>(key)
+        if (data != null) {
+            return data
+        }
+        val value = f()
+        set(key, value, timeout)
+        return value
     }
 }
