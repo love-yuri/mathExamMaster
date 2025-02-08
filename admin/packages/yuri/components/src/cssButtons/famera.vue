7d00449e892d00750576bff62135252b9b71431c
love-yuri <yuri2078170658@gmail.com>
2025-02-08 16:53:32 +0800

    feat: 添加2个炫彩button

---
+++ admin/packages/yuri/components/src/cssButtons/famera.vue
@@ -0,0 +1,53 @@
+<template>
+  <button class="button-73" role="button">
+    <slot></slot>
+  </button>
+</template>
+<style lang="css" scoped>
+
+
+@media (min-width: 768px) {
+  .button-73 {
+    padding: 0.75rem 2rem;
+    font-size: 1.5rem;
+  }
+}
+
+.button-73 {
+  box-sizing: border-box;
+  display: inline-block;
+  padding: 1rem 1.3rem;
+  margin: 0;
+  font-family: -apple-system, sans-serif;
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: #000;
+  text-align: center;
+  text-decoration: none;
+  letter-spacing: -0.24px;
+  appearance: none;
+  touch-action: manipulation;
+  cursor: pointer;
+  quotes: auto;
+  user-select: none;
+  user-select: none;
+  background-color: #fff;
+  border-style: none;
+  border-radius: 40em;
+  outline: none;
+  box-shadow: #adcfff 0 -12px 6px inset;
+  transition: all 0.15s;
+}
+
+.button-73:hover {
+  background-color: #ffc229;
+  box-shadow: #ff6314 0 -6px 8px inset;
+  transform: scale(1.125);
+}
+
+.button-73:active {
+  transform: scale(1.025);
+}
+
+/* CSS */
+</style>
