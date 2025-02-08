d05962b7d990ef7cea485aeb767533f77f198e9b
love-yuri <yuri2078170658@gmail.com>
2025-02-08 15:27:14 +0800

    feat(yuri.api): 添加公用api目录.仅测试api已放入common

---
+++ admin/packages/yuri/api/src/index.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2025-02-07 15:42:32
+ * @LastEditTime: 2025-02-07 16:48:05
+ * @Description:
+ */
+
+// export * from './examPageApi'
+// export * from './examPageReleaseApi'
+// export * from './knowledgePointApi'
+// export * from './questionBankApi'
+// export * from './systemApi'
+// export * from './systemFileApi'
+// export * from './userDepartmentApi'
