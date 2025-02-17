13a836e1d5ea4fca8bbf97bcddb6ed204688d364
love-yuri <yuri2078170658@gmail.com>
2025-02-17 15:54:58 +0800

    feat: 优化选择题选项显示

--- admin/root/src/views/questionManager/questionBank/create/components/singleChoice.vue
+++ admin/root/src/views/questionManager/questionBank/create/components/singleChoice.vue
@@ -58,9 +58,16 @@
       :key="index"
       class="my-2 flex items-center"
     >
-      <div class="mb-2 flex-shrink-0 text-2xl">
-        选项{{ String.fromCharCode(65 + index) }}: &nbsp;
+      <div class="flex-shrink-0 text-2xl">
+        {{ String.fromCharCode(65 + index) }} &nbsp;
       </div>
+      <RadioButton
+        v-model="answer.answer"
+        :input-id="index.toString()"
+        :value="index"
+        class="mr-2"
+        name="dynamic"
+      />
       <InputText
         v-model="item.value"
         class="w-full"
@@ -72,30 +79,6 @@
         severity="danger"
         @click="removeKey(index)"
       />
-    </div>
-
-    <div class="flex flex-col">
-      <div class="mb-2 text-center text-xl font-semibold">
-        请选择正确答案...
-      </div>
-      <div class="flex flex-col gap-4">
-        <div
-          v-for="(item, index) in answer.keys"
-          :key="index"
-          class="flex items-center"
-        >
-          <span class="mr-2 text-xl">{{
-            String.fromCharCode(65 + index)
-          }}</span>
-          <RadioButton
-            v-model="answer.answer"
-            :input-id="index.toString()"
-            :value="index"
-            name="dynamic"
-          />
-          <label :for="item.value" class="ml-2">{{ item.value }}</label>
-        </div>
-      </div>
     </div>
   </div>
 </template>
