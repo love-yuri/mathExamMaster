b507ee0dca43ed371e8464004ecb5dd9abaeb95a
love-yuri <yuri2078170658@gmail.com>
2025-02-24 16:50:28 +0800

    fix: 修复生成测试数据时，查询用户关联关系和题目关联关系的问题

--- server/src/main/resources/sql/generate.sql
+++ server/src/main/resources/sql/generate.sql
@@ -25,3 +25,28 @@ INSERT INTO user VALUE (7, 3, 'grace', 'Grace', 'pass1234', FALSE, NOW(), 'yuri'
 INSERT INTO user VALUE (8, 3, 'henry', 'Henry', 'pass1234', FALSE, NOW(), 'yuri', NOW(), 'yuri');
 INSERT INTO user VALUE (9, 3, 'ivy', 'Ivy', 'pass1234', FALSE, NOW(), 'yuri', NOW(), 'yuri');
 INSERT INTO user VALUE (10, 3, 'jack', 'Jack', 'pass1234', FALSE, NOW(), 'yuri', NOW(), 'yuri');
+
+
+select
+        user_id,
+        epur.id as relation_id,
+        nick_name,
+        user_name
+    from exam_page_release epr
+    left join exam_page_user_relation epur
+    on epr.id = epur.page_release_id
+    left join user on user.id = epur.user_id
+         where epr.id = 1893578089957728258
+
+;
+
+select
+        qb.id,
+        qb.answer
+        from exam_page_user_relation epur
+        left join exam_page_release epr on epr.id = epur.page_release_id
+        left join exam_page_question_relation epqr on epqr.exam_page_id = epr.exam_page_id
+        left join question_bank qb on qb.id = epqr.question_bank_id
+    where epur.id = 1893578090020642826;
+
+select * from exam_page_question_relation where exam_page_id = 1893578020130955265;
