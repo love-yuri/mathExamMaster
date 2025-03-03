e2613d14a47909b4affef6bafa81f475ec03e117
love-yuri <yuri2078170658@gmail.com>
2025-03-03 16:50:08 +0800

    feat: 添加数组比较工具函数，优化代码结构

--- admin/packages/yuri/common/src/utils/commonUtils.ts
+++ admin/packages/yuri/common/src/utils/commonUtils.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2025-02-27 11:55:53
+ * @LastEditTime: 2025-03-03 16:39:55
+ * @Description:
+ */
 /**
  * 防抖函数
  * @param fn - 需要延迟执行的函数
@@ -16,4 +22,18 @@ export function debounce<T extends (...args: any[]) => any>(
       fn(...args); // 延迟后执行函数
     }, delay);
   };
+}
+
+/**
+ * 比较 两个数组是否相同，不关心顺序
+ */
+export function areArraysEqual<T>(arr1: T[], arr2: T[]): boolean {
+  if (arr1.length !== arr2.length) return false;
+
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  return (
+    arr1.every((item) => set2.has(item)) && arr2.every((item) => set1.has(item))
+  );
 }
