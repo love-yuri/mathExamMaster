fecfd3a6f98ba1872cce764d3dfece4fe09ac6b8
love-yuri <yuri2078170658@gmail.com>
2025-03-24 16:54:38 +0800

    feat(QuestionCategoryController): 添加分页查询接口

--- server/src/main/kotlin/math/yl/love/controller/QuestionCategoryController.kt
+++ server/src/main/kotlin/math/yl/love/controller/QuestionCategoryController.kt
@@ -1,14 +1,25 @@
 package math.yl.love.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import math.yl.love.common.base.R
 import math.yl.love.common.mybatis.BaseController
 import math.yl.love.database.domain.entity.QuestionCategory
+import math.yl.love.database.domain.params.BasePageParam
 import math.yl.love.database.mapper.QuestionCategoryMapper
 import math.yl.love.database.service.QuestionCategoryService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/question/category")
 @Tag(name = "题目分类表")
-class QuestionCategoryController: BaseController<QuestionCategory, QuestionCategoryMapper, QuestionCategoryService>()
+class QuestionCategoryController: BaseController<QuestionCategory, QuestionCategoryMapper, QuestionCategoryService>() {
+
+    @PostMapping("page")
+    @Operation(summary = "分页")
+    fun page(@RequestBody param: BasePageParam) = R.success(baseService.page(param.current, param.size))
+
+}
