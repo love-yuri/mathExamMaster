c4dd820ef25f023ecb35da754e9be68e8d4f0025
love-yuri <yuri2078170658@gmail.com>
2025-04-08 15:39:49 +0800

    fix(SystemFile): 修复创建时间和更新时间的序列化注解

--- server/src/main/kotlin/math/yl/love/database/domain/entity/SystemFile.kt
+++ server/src/main/kotlin/math/yl/love/database/domain/entity/SystemFile.kt
@@ -27,7 +27,7 @@ data class SystemFile (
     @Schema(description = "是否被删除")
     val deleted: Boolean = false,
 
-    @Contextual
+    @Serializable(with = JsonConfig.LocalDateTimeSerializer::class)
     @Schema(description = "创建时间")
     @TableField(value = DataBaseConstant.CREATE_TIME, fill = FieldFill.INSERT)
     override val createTime: LocalDateTime? = null,
@@ -36,7 +36,7 @@ data class SystemFile (
     @TableField(value = DataBaseConstant.CREATE_BY, fill = FieldFill.INSERT)
     override val createBy: String? = null,
 
-    @Contextual
+    @Serializable(with = JsonConfig.LocalDateTimeSerializer::class)
     @Schema(description = "更新时间")
     @TableField(value = DataBaseConstant.UPDATE_TIME, fill = FieldFill.INSERT_UPDATE)
     override val updateTime: LocalDateTime? = null,
